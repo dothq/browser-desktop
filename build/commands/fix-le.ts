ca8090bb7b5bea9ff56a44f04dd5316e1f54710e
EnderDev <kieran@dothq.co>
2021-02-10 21:05:05 +0000

    🐛 Make fix-le async

--- build/commands/fix-le.ts
+++ build/commands/fix-le.ts
@@ -2,10 +2,10 @@ import { readdirSync } from "fs-extra";
 import { resolve } from "path";
 import { dispatch } from "../dispatch"
 
-export const fixLineEndings = () => {
+export const fixLineEndings = async () => {
     const patches = readdirSync(resolve(process.cwd(), "patches"));
 
-    patches.forEach(async patch => {
+    await Promise.all(patches.map(async (patch) => {
         await dispatch("dos2unix", [patch], resolve(process.cwd(), "patches"));
-    })
+    }));
 }
