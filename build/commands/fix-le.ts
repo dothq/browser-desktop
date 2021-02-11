dd1136d2846dd64c7321e6fb68dea3e3095f0468
EnderDev <kieran@dothq.co>
2021-02-11 17:26:13 +0000

    💬 Use dos2unix for fixing line endings

--- build/commands/fix-le.ts
+++ build/commands/fix-le.ts
@@ -6,6 +6,6 @@ export const fixLineEndings = async () => {
     const patches = readdirSync(resolve(process.cwd(), "patches"));
 
     await Promise.all(patches.map(async (patch) => {
-        await dispatch("unix2dos", [patch], resolve(process.cwd(), "patches"));
+        await dispatch("dos2unix", [patch], resolve(process.cwd(), "patches"));
     }));
 }
