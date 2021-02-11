b0c7f0fc19b1defb6cbaeb5004d87d565852b9d4
EnderDev <kieran@dothq.co>
2021-02-11 17:29:53 +0000

    🤫 Silence commit

--- windows-init.sh
+++ windows-init.sh
@@ -6,7 +6,7 @@ git config core.autocrlf false
 git checkout --orphan ff
 echo "Adding source files to Git repo. This may take a while..."
 git add . -v > /dev/null
-git commit -am "Firefox"
+git commit -am "Firefox" > /dev/null
 git checkout -b dot
 
 echo "Successfully set up src directory!"
