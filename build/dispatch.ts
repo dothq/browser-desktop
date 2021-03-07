60c29e7f80db2146533838dc2d0819195bbacfd6
EnderDev <kieran@dothq.co>
2021-03-07 19:58:28 +0000

    🐛 Handle stderr data in dispatch

--- build/dispatch.ts
+++ build/dispatch.ts
@@ -1,27 +1,25 @@
 import execa from "execa"
 import { log } from "."
 
+const handle = (data: any) => {
+    const d = data.toString();
+
+    d.split("\n").forEach((line: any) => {
+        if(line.length !== 0) log.info(line)
+    });
+}
+
 export const dispatch = (cmd: string, args?: any[], cwd?: string, noLog?: boolean) => {
     return new Promise((resolve, reject) => {
         if(!noLog) log.info(`Starting child "${cmd} ${args?.join(" ")}".`)
 
         const proc = execa(cmd, args, { cwd: cwd ? cwd : process.cwd() });
 
-        (proc.stdout as any).on('data', (data: any) => {
-            const d = data.toString();
-
-            d.split("\n").forEach((line: any) => {
-                if(line.length !== 0) log.info(line)
-            });
-        });
-
-        (proc.stdout as any).on('error', (data: any) => {
-            const d = data.toString();
-
-            d.split("\n").forEach((line: any) => {
-                if(line.length !== 0) log.info(line)
-            });
-        });
+        proc.stdout?.on('data', handle);
+        proc.stderr?.on('data', handle);
+        
+        proc.stdout?.on('error', handle);
+        proc.stderr?.on('error', handle);
 
         proc.on('exit', () => {
             if(!noLog) log.info(`Done with "${cmd} ${args?.join(" ")}".`)
