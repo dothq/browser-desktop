1432741d9d6cedcf07ab900924ed27337cd42796
EnderDev <kieran@dothq.co>
2021-03-08 21:04:18 +0000

    🐛 Fix issue with fix-le where it died if the patch origin was gone

--- build/commands/fix-le.ts
+++ build/commands/fix-le.ts
@@ -1,11 +1,17 @@
-import { readdirSync } from "fs-extra";
+import { existsSync, readdirSync, readFileSync } from "fs-extra";
 import { resolve } from "path";
+import { log } from "..";
 import { dispatch } from "../dispatch"
 
 export const fixLineEndings = async () => {
-    const patches = readdirSync(resolve(process.cwd(), "patches"));
+    const patchesDir = resolve(process.cwd(), "patches");
+    const patches = readdirSync(patchesDir);
 
     await Promise.all(patches.map(async (patch) => {
-        await dispatch("dos2unix", [patch], resolve(process.cwd(), "patches"));
+        const patchContents = readFileSync(resolve(patchesDir, patch), "utf-8");
+        const originalPath = patchContents.split("diff --git a/")[1].split(" b/")[0];
+
+        if(existsSync(resolve(process.cwd(), "src", originalPath))) await dispatch("dos2unix", [patch], resolve(process.cwd(), "patches"));
+        else log.warning(`Skipping ${patch} as it no longer exists in tree...`);
     }));
 }
