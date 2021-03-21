d12fe3f55e6b1937f9498701bf72c94e7a9d4da5
EnderDev <kieran@dothq.co>
2021-03-21 14:54:15 +0000

    💬 Make CONFIG_GUESS null if src isn't present

--- build/constants/index.ts
+++ build/constants/index.ts
@@ -13,6 +13,8 @@ export const PATCHES_DIR = resolve(process.cwd(), "patches")
 export const COMMON_DIR = resolve(process.cwd(), "common")
 export const CONFIGS_DIR = resolve(process.cwd(), "configs")
 
-export const { stdout: CONFIG_GUESS } = execa.commandSync("./build/autoconf/config.guess", { cwd: SRC_DIR })
+export let CONFIG_GUESS: any = null;
+
+try { CONFIG_GUESS = execa.commandSync("./build/autoconf/config.guess", { cwd: SRC_DIR }).stdout } catch (e) { }
 
 export const OBJ_DIR = resolve(SRC_DIR, `obj-${CONFIG_GUESS}`);
