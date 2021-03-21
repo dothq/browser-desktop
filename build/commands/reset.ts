f6edb4d16a00d84f7a65d39c1bb6aa3f9cd245e7
EnderDev <kieran@dothq.co>
2021-03-21 14:39:22 +0000

    🍉 Reset command now cleans up manual patches

--- build/commands/reset.ts
+++ build/commands/reset.ts
@@ -1,8 +1,12 @@
 import execa from "execa";
+import { exists, existsSync } from "fs-extra";
 import { resolve } from "path";
 import { confirm } from "promptly";
+import rimraf from "rimraf";
 import { bin_name, log } from "..";
 import { SRC_DIR } from "../constants";
+import { IPatch } from "../interfaces/patch";
+import manualPatches from "../manual-patches";
 
 export const reset = async () => {
     try {
@@ -23,6 +27,24 @@ export const reset = async () => {
                         ["checkout", "."],
                         { cwd: SRC_DIR }
                     );
+
+                    manualPatches.forEach(async (patch: IPatch) => {
+                        const { src, action } = patch;
+
+                        if(action == "copy") {
+                            if(typeof(src) == "string") {
+                                const path = resolve(SRC_DIR, src);
+
+                                if(existsSync(path)) rimraf.sync(path)
+                            } else if(Array.isArray(src)) {
+                                src.forEach(i => {
+                                    const path = resolve(SRC_DIR, i);
+
+                                    if(existsSync(path)) rimraf.sync(path)
+                                })
+                            }
+                        }
+                    })
 
                     log.success(
                         "Reset successfully."
