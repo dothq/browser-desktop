3916b7cd994dc616bc12934c93964a17017d0819
EnderDev <kieran@dothq.co>
2021-03-30 20:55:14 +0100

    🐛 Possible fix for #531

--- common/browser/branding/dot/configure.sh
+++ common/browser/branding/dot/configure.sh
@@ -2,9 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-mk_add_options MOZ_APP_VENDOR="Dot HQ"
-mk_add_options MOZ_APP_BASENAME=Dot
-mk_add_options MOZ_APP_PROFILE=dot
-mk_add_options MOZ_APP_DISPLAYNAME="Dot Browser"
-mk_add_options MOZ_MACBUNDLE_ID=co.dothq.browser
-mk_add_options MOZ_DISTRIBUTION_ID=co.dothq.browser
+MOZ_APP_VENDOR="Dot HQ"
+MOZ_APP_BASENAME=Dot
+MOZ_APP_PROFILE=dot
+MOZ_APP_DISPLAYNAME="Dot Browser"
+MOZ_MACBUNDLE_ID=co.dothq.browser
+MOZ_DISTRIBUTION_ID=co.dothq.browser
