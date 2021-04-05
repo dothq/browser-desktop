21e3522a56e73926d2547fe3c3b9fd5ecdc0e2f8
EnderDev <kieran@dothq.co>
2021-04-05 20:14:50 +0100

    🍉 Add command register

---
+++ build/cmds.ts
@@ -0,0 +1,119 @@
+import {
+    bootstrap,
+    build,
+    download,
+    downloadArtifacts,
+    execute,
+    exportFile,
+    exportPatches,
+    fixLineEndings,
+    importPatches,
+    init,
+    licenseCheck,
+    melonPackage,
+    reset,
+    run,
+    status
+} from "./commands";
+
+export const commands = [
+    {
+        cmd: "bootstrap",
+        description: "Bootstrap Dot Browser.",
+        controller: bootstrap
+    },
+    {
+        cmd: "build [os]",
+        aliases: ["b"],
+        description: "Build Dot Browser. Specify the OS param for cross-platform builds.",
+        options: [
+            {
+                arg: "--a, --arch <architecture>",
+                description: "Specify architecture for build"
+            }
+        ],
+        controller: build
+    },
+    {
+        cmd: "download [ffVersion]",
+        description: "Download Firefox.",
+        controller: download
+    },
+    {
+        cmd: "download-artifacts",
+        description: "Download Windows artifacts from GitHub.",
+        flags: {
+            platforms: [
+                "win32"
+            ]
+        },
+        controller: downloadArtifacts
+    },
+    {
+        cmd: "execute",
+        description: "Execute a command inside the src directory.",
+        controller: execute
+    },
+    {
+        cmd: "export-file <file>",
+        description: "Export a changed file as a patch.",
+        controller: exportFile
+    },
+    {
+        cmd: "export",
+        aliases: ["export-patches"],
+        description: "Export the changed files as patches.",
+        controller: exportPatches
+    },
+    {
+        cmd: "lfify",
+        aliases: ["fix-le"],
+        description: "Convert CRLF line endings to Unix LF line endings.",
+        controller: fixLineEndings
+    },
+    {
+        cmd: "import",
+        aliases: ["import-patches", "i"],
+        description: "Import patches into the browser.",
+        options: [
+            {
+                arg: "-m, --minimal",
+                description: "Import patches in minimal mode"
+            }
+        ],
+        controller: importPatches
+    },
+    {
+        cmd: "init",
+        aliases: ["initialise", "initialize"],
+        description: "Initialise the Firefox directory.",
+        controller: init
+    },
+    {
+        cmd: "license-check",
+        aliases: ["lc"],
+        description: "Check the src directory for the absence of MPL-2.0 header.",
+        controller: licenseCheck
+    },
+    {
+        cmd: "package",
+        aliases: ["pack"],
+        description: "Package the browser for distribution.",
+        controller: melonPackage
+    },
+    {
+        cmd: "reset",
+        description: "Reset the source directory to stock Firefox.",
+        controller: reset
+    },
+    {
+        cmd: "run",
+        description: "Run the browser.",
+        controller: run
+    },
+    {
+        cmd: "status",
+        description: "Status and files changed for src directory.",
+        controller: status
+    }
+]
--- build/index.ts
+++ build/index.ts
@@ -1,22 +1,6 @@
 import chalk from "chalk";
-import { Command } from "commander";
-import {
-    bootstrap,
-    build,
-    download,
-    downloadArtifacts,
-    execute,
-    exportFile,
-    exportPatches,
-    fixLineEndings,
-    importPatches,
-    init,
-    licenseCheck,
-    melonPackage,
-    reset,
-    run,
-    status
-} from "./commands";
+import commander, { Command } from "commander";
+import { commands } from "./cmds";
 import Log from "./log";
 import { errorHandler } from "./utils";
 
@@ -43,103 +27,25 @@ program.version(`
 `);
 program.name(bin_name);
 
-program
-    .command("download [ffVersion]")
-    .description("Download Firefox.")
-    .action(download);
+commands.forEach(command => {
+    if (command.flags && !command.flags.platforms.includes(process.platform)) return;
 
-program
-    .command("init <directory>")
-    .alias("initialise")
-    .description("Initialise the Firefox directory.")
-    .action(init);
+    const _cmd = commander.command(command.cmd);
 
-program
-    .command("build [os]")
-    .option('--a, --arch <architecture>', 'Specify architecture for build')
-    .description(
-        "Build Dot Browser. Specify the OS param for cross-platform builds."
-    )
-    .action(build);
+    _cmd.description(command.description);
 
-program
-    .command("export")
-    .alias("export-patches")
-    .description("Export the changed files as patches.")
-    .action(exportPatches);
+    command?.aliases?.forEach(alias => {
+        _cmd.alias(alias);
+    })
 
-program
-    .command("export-file <file>")
-    .description("Export a changed file as a patch.")
-    .action(exportFile);
+    command?.options?.forEach(opt => {
+        _cmd.option(opt.arg, opt.description)
+    });
 
-program
-    .command("import")
-    .alias("import-patches")
-    .alias("i")
-    .option('-m, --minimal', 'Import patches in minimal mode')
-    .description("Import patches into the browser.")
-    .action(importPatches);
+    _cmd.action(command.controller);
 
-program
-    .command("run")
-    .description("Run the browser.")
-    .action(run);
-
-program
-    .command("package")
-    .description("Package the browser for distribution.")
-    .action(melonPackage);
-
-program
-    .command("reset")
-    .description(
-        "Reset the source directory to stock Firefox."
-    )
-    .action(reset);
-
-program
-    .command("status")
-    .description(
-        "Status and files changed for src directory."
-    )
-    .action(status);
-
-program
-    .command("execute")
-    .description(
-        "Execute a command inside the src directory."
-    )
-    .action(execute);
-
-program
-    .command("bootstrap")
-    .description("Bootstrap Dot Browser.")
-    .action(bootstrap);
-
-program
-    .command("fix-le")
-    .description(
-        "Convert CRLF line endings to Unix LF line endings."
-    )
-    .action(fixLineEndings);
-
-program
-    .command("license-check")
-    .alias("lc")
-    .description(
-        "Check the src directory for the absence of MPL-2.0 header"
-    )
-    .action(licenseCheck);
-
-if (process.platform == "win32") {
-    program
-        .command("download-artifacts")
-        .description(
-            "Download Windows artifacts from GitHub."
-        )
-        .action(downloadArtifacts);
-}
+    program.addCommand(_cmd);
+});
 
 process.on("uncaughtException", errorHandler);
 process.on("unhandledException", (err) =>
