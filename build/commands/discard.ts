12ad29ba40ac0623ebd1536b17911d90c952361a
dothq-robot <72629236+dothq-robot@users.noreply.github.com>
2021-04-23 20:40:44 +0000

    🎨 Enforce code style

--- build/commands/discard.ts
+++ build/commands/discard.ts
@@ -11,23 +11,34 @@ interface Options {
 }
 
 const remotes = {
-    ff: (file: string, version: string) => `https://hg.mozilla.org/experimental/firefox-unified-stage/raw-file/FIREFOX_${version.replace(" ", "_").replace(".", "_")}_RELEASE/${file}`,
-    dot: (file: string, ref: string) => `https://raw.githubusercontent.com/dothq/browser-desktop/${ref}/${file}`
-}
+    ff: (file: string, version: string) =>
+        `https://hg.mozilla.org/experimental/firefox-unified-stage/raw-file/FIREFOX_${version
+            .replace(" ", "_")
+            .replace(".", "_")}_RELEASE/${file}`,
+    dot: (file: string, ref: string) =>
+        `https://raw.githubusercontent.com/dothq/browser-desktop/${ref}/${file}`
+};
 
-export const discard = async (file: string, options: Options) => {
+export const discard = async (
+    file: string,
+    options: Options
+) => {
     log.info(`Discarding ${file}...`);
 
-    if (!statSync(file).isFile()) throw new Error("Target must be a file.")
+    if (!statSync(file).isFile())
+        throw new Error("Target must be a file.");
 
     // @todo add remote discard
     if (options.fromRemote) {
-        if (options.fromRemote == "ff" || options.fromRemote == "firefox") {
-
+        if (
+            options.fromRemote == "ff" ||
+            options.fromRemote == "firefox"
+        ) {
         } else if (options.fromRemote == "dot") {
-
         } else {
-            throw new Error("Unrecognised remote type. Expected `ff` or `dot`.")
+            throw new Error(
+                "Unrecognised remote type. Expected `ff` or `dot`."
+            );
         }
     } else {
         if (!existsSync(resolve(SRC_DIR, file)))
@@ -35,34 +46,26 @@ export const discard = async (file: string, options: Options) => {
                 `File ${file} could not be found in src directory. Check the path for any mistakes and try again.`
             );
 
-        const patchFile = resolve(PATCHES_DIR, file.replace(/\//g, "-").replace(/\./g, "-") +
-            ".patch")
+        const patchFile = resolve(
+            PATCHES_DIR,
+            file.replace(/\//g, "-").replace(/\./g, "-") +
+                ".patch"
+        );
 
         if (!existsSync(patchFile))
             throw new Error(
                 `File ${file} does have an associated patch in the patches directory.`
             );
 
-        const {
-            stdout,
-            exitCode
-        } = await execa(
+        const { stdout, exitCode } = await execa(
             "git",
-            [
-                "apply",
-                "-R",
-                "-p",
-                "1",
-                patchFile
-            ],
+            ["apply", "-R", "-p", "1", patchFile],
             { cwd: SRC_DIR }
         );
 
         if (exitCode == 0) {
-            log.success(`Discarded changes to ${file}.`)
+            log.success(`Discarded changes to ${file}.`);
             if (!options.keep) rimraf.sync(patchFile);
-        }
-        else throw new Error(stdout);
+        } else throw new Error(stdout);
     }
-
 };
--- build/commands/index.ts
+++ build/commands/index.ts
@@ -15,4 +15,3 @@ export * from "./reset";
 export * from "./run";
 export * from "./set-branch";
 export * from "./status";
-
