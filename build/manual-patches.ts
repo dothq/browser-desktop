2c0ec53fa512bb800bc29d02f3ea3f579de5772d
Kieran <42723993+EnderDev@users.noreply.github.com>
2021-05-05 00:46:37 +0100

    🔥 Remove SCSS manual import

--- build/manual-patches.ts
+++ build/manual-patches.ts
@@ -74,11 +74,6 @@ const manualPatches: IPatch[] = [
         name: "extensions",
         action: "copy",
         src: ["browser/extensions/shield"]
-    },
-    {
-        name: "build-scss",
-        action: "copy",
-        src: "python/melon"
     }
 ];
 
