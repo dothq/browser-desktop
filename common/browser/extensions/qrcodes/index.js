2495217b1c01306fc5afc0de90f994a1f4b4e8dd
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-05-06 22:20:31 +0100

    💥 Add QR Codes generator for page

--- common/browser/extensions/qrcodes/index.js
+++ common/browser/extensions/qrcodes/index.js
@@ -2,33 +2,135 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log(request, sender, sendResponse)
-});
-
 window.addEventListener("contextmenu", (e) => {
+    if (e.target.nodeName == "INPUT") return;
+
     e.preventDefault();
     e.stopPropagation();
 })
 
-const main = async () => {
-    const getTheme = async () => {
-        const theme = await browser.theme.getCurrent();
-
-        document.getElementById("theme").innerHTML = JSON.stringify(theme);
+// https://stackoverflow.com/a/65620774/10560157
+const throttle = (func, limit) => {
+    let lastFunc;
+    let lastRan = Date.now() - (limit + 1); // enforces a negative value on first run
+    return function (...args) {
+        const context = this;
+        clearTimeout(lastFunc);
+        lastFunc = setTimeout(() => {
+            func.apply(context, args);
+            lastRan = Date.now();
+        }, limit - (Date.now() - lastRan)); // negative values execute immediately
     }
-
-    setInterval(async () => {
-        await getTheme();
-    }, 1000);
-
-    await getTheme();
 }
 
-// I don't see why this is necessary any more. This file will
-// run every time you open the popup, so these listeners are
-// unnecessary
-// browser.tabs.onUpdated.addListener(main);
-// browser.tabs.onActivated.addListener(main);
+const getTheme = async () => {
+    const theme = await browser.theme.getCurrent();
 
-main()
+    for (const [key, value] of Object.entries(theme.colors)) {
+        document.body.style.setProperty(
+            "--theme-" + key.replace(/_/g, "-"),
+            value
+        )
+    }
+}
+
+const getCurrentTab = async () => {
+    const tab = await browser.tabs.query({ currentWindow: true, active: true });
+
+    return tab[0];
+}
+
+const parseHostname = (url) => {
+    const { hostname } = new URL(url);
+
+    return hostname.length == 0 ? url : hostname
+}
+
+const showQrCodeRenderingError = (id) => {
+    document.getElementById("qr-codes-canvas").style.backgroundImage = "";
+
+    const errorEl = document.getElementById("qr-codes-error");
+
+    document.l10n.setAttributes(errorEl, `qr-codes-error-${id}`);
+
+    errorEl.style.display = "";
+}
+
+const renderQrCode = (url) => {
+    document.getElementById("qr-codes-error").style.display = "none";
+
+    if (url.length > 256) return showQrCodeRenderingError("too-long");
+    if (url.length == 0) return showQrCodeRenderingError("too-short");
+
+    const qr = new AwesomeQR.AwesomeQR({
+        text: url,
+        size: 500,
+        colorLight: document.body.style.getPropertyValue("--theme-ntp-background"),
+        colorDark: document.body.style.getPropertyValue("--theme-toolbar-field"),
+        autoColor: false,
+        backgroundDimming: "transparent",
+        components: {
+            data: {
+                scale: 1,
+            },
+            timing: {
+                scale: 1,
+                protectors: true,
+            },
+            alignment: {
+                scale: 1,
+                protectors: true,
+            },
+            cornerAlignment: {
+                scale: 1,
+                protectors: true,
+            },
+        }
+    })
+
+    qr.draw().then(dataUrl => {
+        document.getElementById("qr-codes-canvas").style.backgroundImage = `url(${dataUrl})`;
+    })
+}
+
+const main = async () => {
+    await getTheme();
+
+    const { url } = await getCurrentTab();
+
+    const input = document.getElementById("qr-codes-input");
+    input.value = url;
+
+    const hostname = parseHostname(url);
+    document.l10n.setAttributes(
+        document.getElementById("popup-title"),
+        "qr-codes-title",
+        {
+            hostname
+        }
+    );
+
+    renderQrCode(input.value);
+}
+
+browser.tabs.onUpdated.addListener(async () => {
+    await getTheme();
+    main();
+});
+
+browser.tabs.onActivated.addListener(async () => {
+    await getTheme();
+    main();
+});
+
+browser.theme.onUpdated.addListener(async () => await getTheme());
+
+window.addEventListener("DOMContentLoaded", () => {
+    const input = document.getElementById("qr-codes-input");
+
+    input.addEventListener("keyup", () => {
+        throttle(renderQrCode(input.value), 500)
+    })
+})
+
+main();
