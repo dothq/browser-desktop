68b91782970ef4ff15ac9ca836d7c2d5da7e38e4
trickypr <trickypr@icloud.com>
2021-05-06 19:59:30 +1000

    🐛 Allow qrcode extention to access the current theme

--- common/browser/extensions/qrcodes/background.js
+++ common/browser/extensions/qrcodes/background.js
@@ -2,17 +2,5 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const main = async () => {
-    const getTheme = async () => {
-        const theme = await browser.theme.getCurrent();
-
-        document.getElementById("theme").innerHTML = JSON.stringify(theme);
-    }
-
-    setInterval(async () => {
-        await getTheme();
-    }, 1000);
-}
-
-browser.tabs.onUpdated.addListener(main);
-browser.tabs.onActivated.addListener(main);
+// I have removed all of the code from the background as it is unnecessary
+// It has been moved to index.js
--- common/browser/extensions/qrcodes/index.js
+++ common/browser/extensions/qrcodes/index.js
@@ -10,3 +10,25 @@ window.addEventListener("contextmenu", (e) => {
     e.preventDefault();
     e.stopPropagation();
 })
+
+const main = async () => {
+    const getTheme = async () => {
+        const theme = await browser.theme.getCurrent();
+
+        document.getElementById("theme").innerHTML = JSON.stringify(theme);
+    }
+
+    setInterval(async () => {
+        await getTheme();
+    }, 1000);
+
+    await getTheme();
+}
+
+// I don't see why this is necessary any more. This file will
+// run every time you open the popup, so these listeners are
+// unnecessary
+// browser.tabs.onUpdated.addListener(main);
+// browser.tabs.onActivated.addListener(main);
+
+main()
