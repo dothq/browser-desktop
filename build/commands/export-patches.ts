a4512159b4de7b05c8480e924e1e7da810ccf648
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-05-07 21:55:48 +0100

    🌐 Export patches can now export Fluent locales

--- build/commands/export-patches.ts
+++ build/commands/export-patches.ts
@@ -12,6 +12,7 @@ import { resolve } from "path";
 import { log } from "..";
 import {
     COMMON_DIR,
+
     PATCHES_DIR,
     SRC_DIR
 } from "../constants";
@@ -24,6 +25,13 @@ const flags: {
     M: "modify",
     A: "add"
 };
+
+const isLocale = (l: string) =>
+    (l.endsWith(".inc") && l.includes("locales")) ||
+    l.endsWith(".ftl") ||
+    (l.endsWith(".properties") && !l.includes("test")) ||
+    (l.endsWith(".dtd") && !l.includes("test")) &&
+    l.toLowerCase().includes("en-us")
 
 const getFiles = async (flags: string, cwd: string) => {
     let { stdout: ignored } = await execa(
@@ -51,9 +59,12 @@ const getFiles = async (flags: string, cwd: string) => {
     const files = fls.split("\n").filter((i: any) => {
         return !(
             ignored.split("\n").includes(i) ||
-            i == ".gitignore"
+            i == ".gitignore" ||
+            isLocale(i)
         );
     }); // this filters out the manual patches
+
+    const l10nFiles = fls.split("\n").filter((l: any) => isLocale(l))
 
     log.info(
         `Ignoring ${ignored.split("\n").length} files...`
@@ -69,48 +80,110 @@ const getFiles = async (flags: string, cwd: string) => {
         }
     });
 
-    return { files, fileNames };
+    return { files, fileNames, l10nFiles };
 };
 
 const exportModified = async (
     patchesDir: string,
     cwd: string
 ) => {
-    const { files, fileNames } = await getFiles("M", cwd);
+    const { files, fileNames, l10nFiles } = await getFiles("M", cwd);
+
+    var filesWritten = 0;
 
     await Promise.all(
         files.map(async (file: any, i: any) => {
             if (file) {
-                const proc = execa(
-                    "git",
-                    [
-                        "diff",
-                        "--src-prefix=a/",
-                        "--dst-prefix=b/",
-                        "--full-index",
-                        file
-                    ],
-                    {
-                        cwd,
-                        stripFinalNewline: false
-                    }
-                );
-                const name = fileNames[i];
+                try {
+                    const proc = execa(
+                        "git",
+                        [
+                            "diff",
+                            "--src-prefix=a/",
+                            "--dst-prefix=b/",
+                            "--full-index",
+                            file
+                        ],
+                        {
+                            cwd,
+                            stripFinalNewline: false
+                        }
+                    );
+                    const name = fileNames[i];
 
-                proc.stdout?.pipe(
-                    createWriteStream(
-                        resolve(patchesDir, name)
-                    )
-                );
-                log.info(
-                    `Wrote "${name}" to patches directory.`
-                );
+                    proc.stdout?.pipe(
+                        createWriteStream(
+                            resolve(patchesDir, name)
+                        )
+                    );
 
-                appendFileSync(
-                    resolve(PATCHES_DIR, ".index"),
-                    `${name} - ${file}\n`
-                );
+                    appendFileSync(
+                        resolve(PATCHES_DIR, ".index"),
+                        `${name} - ${file}\n`
+                    );
+
+                    ++filesWritten;
+                } catch (e) {
+                    log.error(e);
+                    return;
+                }
             }
+        })
+    );
+
+    log.info(
+        `Wrote ${filesWritten} to patches directory.`
+    );
+
+    await Promise.all(
+        l10nFiles.map(async (l10n: any, i: any) => {
+            const type = l10n.split(".")[l10n.split(".").length - 1];
+
+            const { stdout: diff } = await execa(
+                "git",
+                [
+                    "diff",
+                    "--src-prefix=a/",
+                    "--dst-prefix=b/",
+                    l10n
+                ],
+                {
+                    cwd,
+                    stripFinalNewline: false
+                }
+            );
+
+            const changed = diff.split("\n").filter(ln => (ln.startsWith("+") && !ln.startsWith("+++"))).map(i => i.substr(1))
+
+            const localePath = l10n.split("/locales/en-US/")[1].split("/");
+            const localeName = (localePath.pop());
+
+            if (!changed) {
+                console.log(diff);
+                log.error(`Failed to find changed lines in ${l10n}`);
+                return;
+            }
+
+            if (changed[0] == "") changed.shift();
+
+            const copyrightNotice = type == "ftl" || type == "properties"
+                ? `# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.\n\n`
+                : type == "dtd"
+                    ? `<!-- This Source Code Form is subject to the terms of the Mozilla Public
+- License, v. 2.0. If a copy of the MPL was not distributed with this
+- file, You can obtain one at http://mozilla.org/MPL/2.0/. -->\n\n`
+                    : ``
+
+            ensureDirSync(resolve(process.cwd(), "l10n", "en-US", ...localePath));
+
+            writeFileSync(
+                resolve(process.cwd(), "l10n", "en-US", ...localePath, localeName),
+                `${copyrightNotice}${changed.join("\n")}`
+            )
+
+            log.info(`Wrote ${changed.length} strings to ${localePath.join("/")}/${localeName}.`)
         })
     );
 };
@@ -149,10 +222,6 @@ const exportManual = async (cwd: string) => {
                         ensureDirSync(outsideSrc); // make sure target dir exists before copying
 
                     copySync(inSrc, outsideSrc);
-
-                    log.info(
-                        `Updated manual patch "${patch.src}".`
-                    );
                 } else if (Array.isArray(patch.src)) {
                     patch.src.forEach((p) => {
                         const inSrc = resolve(cwd, p);
@@ -169,10 +238,6 @@ const exportManual = async (cwd: string) => {
                             ensureDirSync(outsideSrc); // make sure target dir exists before copying
 
                         copySync(inSrc, outsideSrc);
-
-                        log.info(
-                            `Updated manual patch "${p}".`
-                        );
                     });
                 }
             }
@@ -187,6 +252,8 @@ export const exportPatches = async () => {
     console.log();
     rmdirSync(PATCHES_DIR, { recursive: true });
     mkdirSync(PATCHES_DIR);
+    rmdirSync(resolve(process.cwd(), "l10n", "en-US"), { recursive: true });
+    mkdirSync(resolve(process.cwd(), "l10n", "en-US"));
     writeFileSync(resolve(PATCHES_DIR, ".index"), "");
 
     log.info("Exporting modified files...");
