06539dcd0a1ec759c7c0d4936f2cfa56c48cb7ae
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-07-11 21:10:36 +0100

    🥅 Catch timeout error axios throws

--- build/middleware/update-check.ts
+++ build/middleware/update-check.ts
@@ -7,20 +7,20 @@ export const updateCheck = async () => {
     const firefoxVersion =
         pjson.versions["firefox-display"];
 
-    const { data } = await axios.get(
-        `https://product-details.mozilla.org/1.0/firefox_history_major_releases.json`,
-        { timeout: 1000 }
-    );
+    try {
+        const { data } = await axios.get(
+            `https://product-details.mozilla.org/1.0/firefox_history_major_releases.json`,
+            { timeout: 1000 }
+        )
 
-    if (data) {
-        let version =
-            Object.keys(data)[
-                Object.keys(data).length - 1
-            ];
+        if (data) {
+            let version =
+                Object.keys(data)[Object.keys(data).length - 1];
 
-        if (firefoxVersion && version !== firefoxVersion)
-            log.warning(
-                `Latest version of Firefox (${version}) does not match frozen version (${firefoxVersion}).`
-            );
-    }
+            if (firefoxVersion && version !== firefoxVersion)
+                log.warning(
+                    `Latest version of Firefox (${version}) does not match frozen version (${firefoxVersion}).`
+                );
+        }
+    } catch(e) {}
 };
