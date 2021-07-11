a281b3dc0eaa5b4b42e519ec70424a4d16e7a7e6
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-07-11 21:06:29 +0100

    ⏰ Add timeout to update check middleware

--- build/middleware/update-check.ts
+++ build/middleware/update-check.ts
@@ -8,14 +8,17 @@ export const updateCheck = async () => {
         pjson.versions["firefox-display"];
 
     const { data } = await axios.get(
-        `https://product-details.mozilla.org/1.0/firefox_history_major_releases.json`
+        `https://product-details.mozilla.org/1.0/firefox_history_major_releases.json`,
+        { timeout: 1000 }
     );
 
-    let version =
-        Object.keys(data)[Object.keys(data).length - 1];
+    if (data) {
+        let version =
+            Object.keys(data)[Object.keys(data).length - 1];
 
-    if (firefoxVersion && version !== firefoxVersion)
-        log.warning(
-            `Latest version of Firefox (${version}) does not match frozen version (${firefoxVersion}).`
-        );
+        if (firefoxVersion && version !== firefoxVersion)
+            log.warning(
+                `Latest version of Firefox (${version}) does not match frozen version (${firefoxVersion}).`
+            );
+    }
 };
