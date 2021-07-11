58c856d9ddaf8b6848ca681189d9c35f64c204ce
dothq-robot <72629236+dothq-robot@users.noreply.github.com>
2021-07-11 20:11:07 +0000

    🎨 Enforce code style

--- build/middleware/update-check.ts
+++ build/middleware/update-check.ts
@@ -11,16 +11,21 @@ export const updateCheck = async () => {
         const { data } = await axios.get(
             `https://product-details.mozilla.org/1.0/firefox_history_major_releases.json`,
             { timeout: 1000 }
-        )
+        );
 
         if (data) {
             let version =
-                Object.keys(data)[Object.keys(data).length - 1];
+                Object.keys(data)[
+                    Object.keys(data).length - 1
+                ];
 
-            if (firefoxVersion && version !== firefoxVersion)
+            if (
+                firefoxVersion &&
+                version !== firefoxVersion
+            )
                 log.warning(
                     `Latest version of Firefox (${version}) does not match frozen version (${firefoxVersion}).`
                 );
         }
-    } catch(e) {}
+    } catch (e) {}
 };
