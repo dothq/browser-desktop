b2c166c439ef72a39131333012c26f7f0184b826
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-07-17 14:23:03 +0100

    💄 Add WindowControls component to UI

--- src/dot/components/Chrome/index.tsx
+++ src/dot/components/Chrome/index.tsx
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { WindowControls } from "../WindowControls"
+
 export const Chrome = () => {
     return (
         <div id={"navigator-toolbox"}>
@@ -9,6 +11,7 @@ export const Chrome = () => {
                 <div id={"navigation-bar-container"}>
                     sdfgadfgidfsjg
                 </div>
+                <WindowControls />
             </nav>
             <nav id={"tab-bar"}>
                 bingus
