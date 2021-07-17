2aab8444b1a053d08a8107c2a3c46c37b0018d27
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-07-17 14:22:36 +0100

    ✨ Add native window controls

---
+++ src/dot/components/WindowControls/index.tsx
@@ -0,0 +1,11 @@
+export const WindowControls = () => {
+    return (
+        <div class="titlebar-buttonbox-container">
+            <div class="titlebar-buttonbox titlebar-color">
+                <a class="titlebar-button titlebar-min"></a>
+                <a class="titlebar-button titlebar-max"></a>
+                <a class="titlebar-button titlebar-close"></a>
+            </div>
+        </div>
+    )
+}
