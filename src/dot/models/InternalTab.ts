c9d4a0b781772ab7aa021534d1c7d1deffb327c1
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-07-26 16:16:33 +0100

    🎉 Start work on InternalTabs

--- src/dot/models/InternalTab.ts
+++ src/dot/models/InternalTab.ts
@@ -1,5 +1,17 @@
+import { builtInPageFavicons } from "../shared/built-in";
 import { Tab } from "./Tab";
 
 export class InternalTab extends Tab {
     public internal: boolean = true;
+
+    constructor({ id }: { id: string }) {
+        super({
+            url: "about:blank",
+            background: false,
+            internal: true
+        })
+
+        this.faviconUrl = builtInPageFavicons[id];
+        this.title = ""
+    }
 }
--- src/dot/shared/built-in.ts
+++ src/dot/shared/built-in.ts
@@ -9,9 +9,13 @@ export const builtInExtensions = [
         version: 1.2,
         mount: "resource://builtin-themes/dark/"
     },
-        {
+    {
         id: "fusion@themes.dothq.co",
         version: 1.0,
         mount: "resource://builtin-themes/fusion/"
     }
 ]
+
+export const builtInPageFavicons: { [key: string]: string } = {
+    settings: "chrome://dot/content/skin/icons/settings.svg"
+}
