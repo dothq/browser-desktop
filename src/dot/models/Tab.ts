13d449cc86bff9e97fd0d5071f6a3d8e46e63ca6
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-07-26 13:15:29 +0100

    ♻️ Refractor Tab model

--- src/dot/models/Tab.ts
+++ src/dot/models/Tab.ts
@@ -2,11 +2,13 @@ import { dot } from "../api";
 import { store } from "../app/store";
 import { Cc, ChromeUtils, Ci, Services } from "../modules";
 import { WELCOME_SCREEN_URL_PARSED } from "../shared/tab";
+import { whitelistedSchemes } from "../shared/url";
 import { MozURI } from "../types/uri";
 
 export interface ITab {
     url: string,
-    background?: boolean
+    background?: boolean,
+    internal?: boolean
 }
 
 export class Tab {
@@ -14,7 +16,23 @@ export class Tab {
 
     public background: boolean;
 
-    public state: 'loading' | 'idle' | 'unknown' = 'unknown';
+    private _state: 'loading' | 'idle' | 'unknown' = "loading";
+
+    public get state() {
+        return this._state;
+    }
+
+    public set state(state: 'loading' | 'idle' | 'unknown') {
+        this._state = state;
+
+        store.dispatch({
+            type: "TAB_UPDATE_STATE",
+            payload: {
+                id: this.id,
+                state
+            }
+        });
+    }
 
     public url = "about:blank";
 
@@ -27,6 +45,8 @@ export class Tab {
         hash: null,
         internal: true
     };
+
+    public internal: boolean = false;
 
     public get active() {
         return dot.tabs.selectedTabId == this.id;
@@ -64,7 +84,23 @@ export class Tab {
         'extension' |
         'file' = "search"
 
-    public title: string = "";
+    private _title: string = "";
+
+    public get title() {
+        return this._title;
+    }
+
+    public set title(title: string) {
+        this._title = title;
+
+        store.dispatch({
+            type: "TAB_UPDATE_TITLE",
+            payload: {
+                id: this.id,
+                title
+            }
+        });
+    }
 
     public faviconUrl: any;
 
@@ -72,136 +108,74 @@ export class Tab {
 
     constructor({
         url,
-        background
+        background,
+        internal
     }: ITab) {
         const browser = dot.browsersPrivate.create({
-            type: "content",
-            context: "contentAreaContextMenu",
-            tooltip: "aHTMLTooltip",
-            autocompletepopup: "PopupAutoComplete",
-            selectmenulist: "ContentSelectDropdown",
-            message: true,
-            messagemanagergroup: "browsers",
-            remote: true,
-            maychangeremoteness: true,
-
-            background: !!background
-        }, Services.io.newURI(url));
+            background: !!background,
+            internal: this.internal || false
+        }, internal ? undefined : Services.io.newURI(url));
 
         this.webContents = browser;
         this.id = this.webContents.browserId;
 
         this.background = !!background;
 
-        const tab = this;
+        this.createProgressListener();
 
+        this.webContents.addEventListener(
+            "WillChangeBrowserRemoteness",
+            this.onBrowserRemoteChange
+        );
+
+        this.webContents.addEventListener(
+            "pagetitlechanged",
+            this.onPageTitleChange
+        );
+
+        this.webContents.addEventListener(
+            "contextmenu",
+            (event: MouseEvent) => dot.menu.get("context-navigation")?.toggle(event)
+        );
+
+        return this;
+    }
+
+    public goto(uri: MozURI, options?: any) {
+        dot.browsersPrivate.goto(this.id, uri, options);
+    }
+
+    public goBack() {
+        this.webContents.goBack();
+        this.updateNavigationState();
+    }
+
+    public goForward() {
+        this.webContents.goForward();
+        this.updateNavigationState();
+    }
+
+    public reload(flags?: number) {
+        if (!flags) {
+            flags = Ci.nsIWebNavigation.LOAD_FLAGS_NONE;
+        };
+
+        this.webContents.reloadWithFlags(flags);
+        this.updateNavigationState();
+    }
+
+    public destroy() {
+        this.webContents.destroy();
+        this.webContents.remove();
+    }
+
+    public select() {
+        dot.browsersPrivate.select(this.id);
+    }
+
+    public createProgressListener() {
         const progressListener = {
-            onStateChange(webProgress: any, request: any, flags: number, status: any) {
-                if (!request) return;
-
-                const url = request.QueryInterface(Ci.nsIChannel).originalURI.spec;
-
-                if (url == "about:blank") {
-                    return;
-                };
-
-                () => tab.updateNavigationState();
-
-                let state = "unknown";
-
-                if (request.isLoadingDocument) state = "loading"
-                else state = "idle"
-
-                store.dispatch({
-                    type: "TAB_UPDATE_STATE",
-                    payload: {
-                        id: tab.id,
-                        state
-                    }
-                });
-
-                console.log("onStateChange", webProgress, request.loadInfo, flags, status);
-            },
-
-            onLocationChange(progress: any, request: any, location: MozURI, flags: number) {
-                if (!progress.isTopLevel) return;
-
-                // Ignore the initial about:blank, unless about:blank is requested
-                if (request) {
-                    const url = request.QueryInterface(Ci.nsIChannel).originalURI.spec;
-                    if (location.spec == "about:blank" && url != "about:blank") return;
-                }
-
-                const whitelistedSchemes = [
-                    "http",
-                    "https",
-                    "ws",
-                    "wss",
-                    "file",
-                    "ftp",
-                    "moz-extension",
-                    "chrome",
-                    "resource",
-                    "moz",
-                    "moz-icon",
-                    "moz-gio"
-                ]
-
-                const isHttp = location.scheme.startsWith("http");
-                const rootDomain = isHttp ? Services.eTLD.getBaseDomainFromHost(location.host) : "";
-                const notWhitelisted = !whitelistedSchemes.includes(location.scheme);
-                const noTrailingPath = location.filePath.replace(/\/*$/, "");
-
-                let pageState = "info";
-
-                if (location.spec == WELCOME_SCREEN_URL_PARSED.spec)
-                    pageState = "search"
-                else if (location.scheme == "https") pageState = "https"
-                else if (location.scheme == "http") pageState = "http"
-                else if (location.scheme == "moz-extension") pageState = "extension"
-                else if (
-                    location.scheme == "about"
-                ) pageState = "built-in"
-                else if (
-                    location.scheme == "file" ||
-                    location.scheme == "chrome" ||
-                    location.scheme == "resource"
-                ) pageState = "file"
-
-                const scheme = whitelistedSchemes.includes(location.scheme)
-                    ? `${location.scheme}://`
-                    : `${location.scheme}:`
-
-                store.dispatch({
-                    type: "TAB_UPDATE",
-                    payload: {
-                        id: tab.id,
-                        url: location.spec,
-                        pageState,
-                        urlParts: {
-                            scheme,
-                            domain: notWhitelisted
-                                ? location.pathQueryRef
-                                : rootDomain,
-                            host: notWhitelisted
-                                ? ""
-                                : location.host.replace(rootDomain, ""),
-                            path: notWhitelisted
-                                ? ""
-                                : noTrailingPath,
-                            query: notWhitelisted
-                                ? ""
-                                : location.query ? "?" + location.query : "",
-                            hash: notWhitelisted
-                                ? ""
-                                : location.ref ? "#" + location.ref : "",
-                            internal: !isHttp
-                        }
-                    }
-                });
-
-                dot.titlebar.emit(`page-location-change`, tab.id);
-            },
+            ...this,
 
             QueryInterface: ChromeUtils.generateQI([
                 "nsIWebProgressListener",
@@ -217,90 +191,126 @@ export class Tab {
         dot.tabs.tabListeners.set(this.id, progressListener);
         dot.tabs.tabFilters.set(this.id, filter);
 
-        tab.webContents.webProgress.addProgressListener(
+        this.webContents.webProgress.addProgressListener(
             filter,
             Ci.nsIWebProgress.NOTIFY_ALL
         );
+    }
 
-        this.webContents.addEventListener("WillChangeBrowserRemoteness", (event: any) => {
-            let { id } = event.originalTarget;
-            let tab: any = dot.tabs.get(id);
-            if (!tab) {
-                return;
-            }
+    public onStateChange(webProgress: any, request: any, flags: number, status: any) {
+        if (!request) return;
 
-            // Dispatch the `BeforeTabRemotenessChange` event, allowing other code
-            // to react to this tab's process switch.
-            let evt = document.createEvent("Events");
-            evt.initEvent("BeforeTabRemotenessChange", true, false);
-            tab.dispatchEvent(evt);
+        const url = request.QueryInterface(Ci.nsIChannel).originalURI.spec;
+        if (url == "about:blank") return;
 
-            // Unhook our progress listener.
-            let filter = dot.tabs.tabFilters.get(id);
-            let oldListener = dot.tabs.tabListeners.get(id);
+        () => this.updateNavigationState();
 
-            this.webContents.webProgress.removeProgressListener(filter);
-            filter.removeProgressListener(oldListener);
+        if (request.isLoadingDocument) this.state = "loading"
+        else this.state = "idle"
+    }
 
-            // We'll be creating a new listener, so destroy the old one.
-            oldListener = null;
-        });
+    public onLocationChange(progress: any, request: any, location: MozURI, flags: number) {
+        if (!progress.isTopLevel) return;
 
-        this.webContents.addEventListener("pagetitlechanged", (event: any) => {
-            if (this.state !== "idle") return;
+        // Ignore the initial about:blank, unless about:blank is requested
+        if (request) {
+            const url = request.QueryInterface(Ci.nsIChannel).originalURI.spec;
+            if (location.spec == "about:blank" && url != "about:blank") return;
+        }
 
-            // Ignore empty title changes on internal pages. This prevents the title
-            // from changing while Fluent is populating the (initially-empty) title
-            // element.
-            if (
-                !browser.contentTitle &&
-                browser.contentPrincipal.isSystemPrincipal
-            ) return;
+        const isHttp = location.scheme.startsWith("http");
+        const rootDomain = isHttp ? Services.eTLD.getBaseDomainFromHost(location.host) : "";
+        const notWhitelisted = !whitelistedSchemes.includes(location.scheme);
+        const noTrailingPath = location.filePath.replace(/\/*$/, "");
 
-            if (this.title == browser.contentTitle) return;
+        let pageState = "info";
 
-            store.dispatch({
-                type: "TAB_UPDATE_TITLE",
-                payload: {
-                    id: this.id,
-                    title: browser.contentTitle
+        if (location.spec == WELCOME_SCREEN_URL_PARSED.spec)
+            pageState = "search"
+        else if (location.scheme == "https") pageState = "https"
+        else if (location.scheme == "http") pageState = "http"
+        else if (location.scheme == "moz-extension") pageState = "extension"
+        else if (
+            location.scheme == "about"
+        ) pageState = "built-in"
+        else if (
+            location.scheme == "file" ||
+            location.scheme == "chrome" ||
+            location.scheme == "resource"
+        ) pageState = "file"
+
+        const scheme = whitelistedSchemes.includes(location.scheme)
+            ? `${location.scheme}://`
+            : `${location.scheme}:`
+
+        store.dispatch({
+            type: "TAB_UPDATE",
+            payload: {
+                id: this.id,
+                url: location.spec,
+                pageState,
+                urlParts: {
+                    scheme,
+                    domain: notWhitelisted
+                        ? location.pathQueryRef
+                        : rootDomain,
+                    host: notWhitelisted
+                        ? ""
+                        : location.host.replace(rootDomain, ""),
+                    path: notWhitelisted
+                        ? ""
+                        : noTrailingPath,
+                    query: notWhitelisted
+                        ? ""
+                        : location.query ? "?" + location.query : "",
+                    hash: notWhitelisted
+                        ? ""
+                        : location.ref ? "#" + location.ref : "",
+                    internal: !isHttp
                 }
-            });
+            }
         });
-
-        this.webContents.addEventListener("contextmenu", (event: MouseEvent) => {
-            dot.menu.get("context-navigation")?.toggle(event);
-        });
-
-        return this;
     }
 
-    goto(uri: MozURI, options?: any) {
-        dot.browsersPrivate.goto(this.id, uri, options);
+    public onBrowserRemoteChange(event: any) {
+        let { id } = event.originalTarget;
+        let tab: any = dot.tabs.get(id);
+        if (!tab) {
+            return;
+        }
+
+        // Dispatch the `BeforeTabRemotenessChange` event, allowing other code
+        // to react to this tab's process switch.
+        let evt = document.createEvent("Events");
+        evt.initEvent("BeforeTabRemotenessChange", true, false);
+        tab.dispatchEvent(evt);
+
+        // Unhook our progress listener.
+        let filter = dot.tabs.tabFilters.get(id);
+        let oldListener = dot.tabs.tabListeners.get(id);
+
+        this.webContents.webProgress.removeProgressListener(filter);
+        filter.removeProgressListener(oldListener);
+
+        // We'll be creating a new listener, so destroy the old one.
+        oldListener = null;
     }
 
-    goBack() {
-        this.webContents.goBack();
-    }
+    public onPageTitleChange() {
+        if (this.state !== "idle") return;
 
-    goForward() {
-        this.webContents.goForward();
-    }
+        const browser = this.webContents;
 
-    reload(flags?: number) {
-        if (!flags) {
-            flags = Ci.nsIWebNavigation.LOAD_FLAGS_NONE;
-        };
+        // Ignore empty title changes on internal pages. This prevents the title
+        // from changing while Fluent is populating the (initially-empty) title
+        // element.
+        if (
+            !browser.contentTitle &&
+            browser.contentPrincipal.isSystemPrincipal
+        ) return;
 
-        this.webContents.reloadWithFlags(flags);
-    }
+        if (this.title == browser.contentTitle) return;
 
-    destroy() {
-        this.webContents.destroy();
-        this.webContents.remove();
-    }
-
-    select() {
-        dot.browsersPrivate.select(this.id);
+        this.title = browser.contentTitle;
     }
 }
