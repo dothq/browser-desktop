b31db3211367fcf35a6056993493286357287be4
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-07-27 22:20:14 +0100

    🔥 Remove old Menu API

--- src/dot/api/menu.ts
+++ src/dot/api/menu.ts
@@ -1,32 +1,5 @@
-import { ContextMenuProps } from "../menus";
 import { Menu } from "../models/Menu";
 
 export class MenuAPI {
     public list: Menu[] = []
-
-    public register(data: ContextMenuProps) {
-        const menu = new Menu(data.id);
-
-        this.list.push(menu);
-    }
-
-    public get(menuId: string) {
-        return this.list.find(menu => menu.id == menuId);
-    }
-
-    public open(menuId: string, x?: number, y?: number) {
-        const menu = this.get(menuId);
-        
-        if (menu) {
-            menu.open(x || 0, y || 0);
-        }
-    }
-
-    public close(menuId: string) {
-        const menu = this.get(menuId);
-        
-        if (menu) {
-            menu.close();
-        }
-    }
 }
