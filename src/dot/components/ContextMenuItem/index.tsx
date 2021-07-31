051e425a395518fe006b2b570806ffe995d52dd0
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-07-31 21:17:31 +0100

    💄 Fix context menu styles

--- src/dot/components/ContextMenuItem/index.tsx
+++ src/dot/components/ContextMenuItem/index.tsx
@@ -1,13 +1,34 @@
 import React from "react";
+import { dot } from "../../api";
 import { MenuItem } from "../../menus";
 
 export const ContextMenuItem = ({
     id,
     label,
-    icon
+    icon,
+    hotkey,
+    hover,
+    click,
+    disabled,
+    visible,
+    type
 }: MenuItem) => {
+    if (type == "separator") return <hr className={"contextmenu-separator"}></hr>;
+
+    const onClick = () => {
+        if (click) click();
+        dot.menus.clear(true);
+    }
+
     return (
-        <div className={"contextmenu-item"} id={id}>
+        <div
+            className={"contextmenu-item"}
+            id={id}
+            onMouseOver={hover}
+            onClick={onClick}
+            data-disabled={disabled}
+            data-visible={visible}
+        >
             <i
                 className={"contextmenu-item-icon"}
                 style={{
@@ -18,6 +39,10 @@ export const ContextMenuItem = ({
             <label className={"contextmenu-item-label"}>
                 {label}
             </label>
+
+            {hotkey && <label className={"contextmenu-item-keybind"}>
+                {hotkey.join("+")}
+            </label>}
         </div>
     )
 }
