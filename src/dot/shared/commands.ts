673b3aacbda2ca0cccaaa3de06c96b656d903c71
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-08-02 22:36:04 +0100

    ✨ Add preferences commands

--- src/dot/shared/commands.ts
+++ src/dot/shared/commands.ts
@@ -7,13 +7,23 @@ export const commands: any = {
    "Browser:GoForward": () => dot.tabs.selectedTab?.goForward(),
    "Browser:Reload": () => dot.tabs.selectedTab?.reload(),
 
-   "Browser:Bookmark": () => store.dispatch({ type: "TAB_BOOKMARK", payload: {
-      id: dot.tabs.selectedTabId
-   }}),
+   "Browser:Bookmark": () => store.dispatch({
+      type: "TAB_BOOKMARK", payload: {
+         id: dot.tabs.selectedTabId
+      }
+   }),
 
-   "Browser:NewTab": () => store.dispatch({ type: "TAB_CREATE", payload: {
-      url: WELCOME_SCREEN_URL
-   }}),
+   "Browser:NewTab": () => store.dispatch({
+      type: "TAB_CREATE", payload: {
+         url: WELCOME_SCREEN_URL
+      }
+   }),
+
+   "Browser:OpenPreferences": () => store.dispatch({
+      type: "TAB_CREATE_INTERNAL", payload: {
+         id: "settings"
+      }
+   }),
 
    "Browser:LaunchBrowserToolbox": () => dot.dev.launchBrowserToolbox()
 }
