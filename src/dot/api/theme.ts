6309e33c433a5704b41abcc4ba2e4dd673aef189
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-08-02 22:31:50 +0100

    🚩 Add theme shade flags for accent colour correction

--- src/dot/api/theme.ts
+++ src/dot/api/theme.ts
@@ -203,8 +203,86 @@ function _isColorDark(r: number, g: number, b: number) {
   return 0.2125 * r + 0.7154 * g + 0.0721 * b <= 110;
 }
 
+const hexToRGB = (hex: string) => {
+  if (hex.length !== 6) return [255, 255, 255];
+
+  const parts = hex.match(/.{1,2}/g);
+
+  if (!parts) return [255, 255, 255];
+
+  return [
+    parseInt(parts[0], 16),
+    parseInt(parts[1], 16),
+    parseInt(parts[2], 16)
+  ];
+}
+
+const hslToRGB = (h: number, s: number, l: number) => {
+  var r, g, b;
+
+  if (s == 0) {
+    r = g = b = l; // achromatic
+  } else {
+    const hue2rgb = (p: number, q: number, t: number) => {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1 / 6) return p + (q - p) * 6 * t;
+      if (t < 1 / 2) return q;
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+      return p;
+    }
+
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
+
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
+
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+const toRGB = (colour: string): number[] => {
+  let type: 'hex' | 'hsl' | 'rgb' | undefined;
+
+  if (colour.startsWith("#")) type = "hex";
+  else if (colour.match(/0[xX][0-9a-fA-F]+/)) type = "hex";
+  else if (colour.startsWith("rgb")) type = "rgb";
+  else if (colour.startsWith("hsl")) type = "hsl";
+  else {
+    return [255, 255, 255];
+  }
+
+  switch (type) {
+    case "hex":
+      return hexToRGB(colour.replace(/#/, ""));
+    case "rgb":
+      return colour
+        .replace(/rgb\(/g, "")
+        .replace(/\)/g, "")
+        .split(",")
+        .map(i => parseInt(i));
+    case "hsl":
+      const hsl = colour
+        .replace(/rgb\(/g, "")
+        .replace(/\)/g, "")
+        .split(",")
+        .map(i => parseInt(i));
+
+      const h = hsl[0];
+      const s = hsl[1];
+      const l = hsl[2];
+
+      return hslToRGB(h, s, l);
+    default:
+      return [255, 255, 255];
+  }
+}
+
 export class ThemeAPI {
   public isSystemDarkMode = false;
+  public isThemeDynamic: boolean = false;
 
   private _currentTheme: any = null;
   private _currentThemeExperiments: any = null;
@@ -256,6 +334,8 @@ export class ThemeAPI {
       this._currentThemeId = themeData.theme.id;
       this._currentThemeExperiments = this._currentTheme.experimental;
 
+      this.isThemeDynamic = !!themeData.darkTheme;
+
       const mapped = [...ThemeVariableMap, ...toolkitVariableMap].map((i: any) => {
         return { variable: i[0], data: i[1] }
       });
@@ -295,7 +375,7 @@ export class ThemeAPI {
         };
       }
 
-      dot.window.addWindowClass(`theme-${this._currentThemeId}`);
+      this.determineDarkness();
     } else {
       this.setTheme(Services.builtInThemes.DEFAULT_THEME_ID);
     }
@@ -322,6 +402,41 @@ export class ThemeAPI {
         },
       });
     });
+  }
+
+  public determineDarkness() {
+    dot.window.removeWindowClassByNamespace(`theme-is-`, document.documentElement);
+
+    if (this.isThemeDynamic) {
+      return dot.window.addWindowClass(
+        `theme-is-dynamic`,
+        true,
+        document.documentElement
+      );
+    };
+
+    const points = [
+      this.theme.toolbarColor,
+      this.theme.accentcolor
+    ];
+
+    for (const point of points) {
+      const rgb = toRGB(point);
+
+      if (_isColorDark(rgb[0], rgb[1], rgb[2])) {
+        dot.window.addWindowClass(
+          `theme-is-dark`,
+          true,
+          document.documentElement
+        );
+      } else {
+        dot.window.addWindowClass(
+          `theme-is-light`,
+          true,
+          document.documentElement
+        );
+      }
+    }
   }
 
   constructor() {
