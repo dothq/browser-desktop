40425e34c410d57582a9cffcb360dcc7357171f8
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-08-02 22:36:12 +0100

    💬 Add built in page titles

--- src/dot/shared/built-in.ts
+++ src/dot/shared/built-in.ts
@@ -2,20 +2,30 @@ export const builtInExtensions = [
     {
         id: "light@themes.dothq.co",
         version: 1.2,
-        mount: "resource://builtin-themes/light/"
+        mount: "resource://builtin-themes/light/",
+        type: "theme"
     },
     {
         id: "dark@themes.dothq.co",
         version: 1.2,
-        mount: "resource://builtin-themes/dark/"
+        mount: "resource://builtin-themes/dark/",
+        type: "theme"
     },
     {
         id: "fusion@themes.dothq.co",
         version: 1.0,
-        mount: "resource://builtin-themes/fusion/"
+        mount: "resource://builtin-themes/fusion/",
+        type: "theme"
     }
 ]
 
 export const builtInPageFavicons: { [key: string]: string } = {
     settings: "chrome://dot/content/skin/icons/settings.svg"
 }
+
+export const builtInPageTitles: { [key: string]: string } = {
+    settings: "Settings"
+}
+
+// privilaged built in pages will have access to the accent colour
+export const builtInPrivilaged = ["settings"];
