33420014b702f680dee0699fc17c68745546bdf4
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-08-05 21:24:54 +0100

    💬 Add breadcrumbs to settings section titles

--- src/dot/resources/shared/components/Section/index.tsx
+++ src/dot/resources/shared/components/Section/index.tsx
@@ -1,9 +1,14 @@
 import React from "react"
+import { ToolbarButton } from "../../../../components/ToolbarButton"
 
-export const Section = ({ title, children }: { title: string; children?: any }) => {
+export const Section = ({ title, children, breadcrumbs }: { title: string; children?: any, breadcrumbs?: string[] }) => {
     return (
         <section className={"webui-section"}>
-            <h1 className={"webui-section-title"}>{title}</h1>
+            <h1 className={"webui-section-title"}>
+                {breadcrumbs && <ToolbarButton image={"chrome://dot/content/skin/icons/back.svg"} />}
+                {title}
+                {breadcrumbs?.join(" / ")}
+            </h1>
 
             {children}
         </section>
