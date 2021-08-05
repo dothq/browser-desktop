6d926482ac4ec53df830309d637bb6fbd55d292f
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-08-05 21:24:02 +0100

    ✨ Add danger state to Buttons

---
+++ src/dot/resources/settings/sections/appearance/theme-editor/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Section } from "../../../../shared/components/Section";
+
+export const ThemeEditor = () => {
+    return (
+        <Section title={"Appearance"} breadcrumbs={["Themes", "Edit Theme"]}>
+
+        </Section>
+    );
+}
