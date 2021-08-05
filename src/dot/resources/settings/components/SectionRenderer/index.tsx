e512bf2af55afaa12ea1ad0d7f71b50ed5bc5e61
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-08-05 21:21:55 +0100

    💄 Register theme editor appearance section

--- src/dot/resources/settings/components/SectionRenderer/index.tsx
+++ src/dot/resources/settings/components/SectionRenderer/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useBrowserSelector } from "../../../../app/store/hooks";
 import { Appearance } from "../../sections/appearance";
 import { General } from "../../sections/general";
+import { ThemeEditor } from "../ThemeEditor";
 
 export const SectionRenderer = () => {
     const settings = useBrowserSelector(s => s.settings);
@@ -10,6 +11,7 @@ export const SectionRenderer = () => {
         <>
             {settings.selectedSection == "general" && <General />}
             {settings.selectedSection == "appearance" && <Appearance />}
+            {settings.selectedSection == "appearance.theme-editor" && <ThemeEditor />}
         </>
     )
 }
