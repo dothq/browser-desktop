44e4394f805332b5e96976ba3f7e7913dc22018e
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-08-06 14:17:21 +0100

    🐛 Adjust patch targets for patch doctor

--- scripts/doctors/patches.py
+++ scripts/doctors/patches.py
@@ -5,12 +5,12 @@ print("🏥 Running patches doctor...")
 print("\nThis doctor only checks two major files required for Dot Browser to build. You may want to check the other files.\n")
 
 try:
-    check1 = open('engine/browser/app/profile/firefox.js')
+    check1 = open('engine/Makefile.in')
     check2 = open('engine/browser/confvars.sh')
 except FileNotFoundError:
     sys.exit('❌ Patches failed to apply. Missing source files.')
 
-if (('Dot Browser' in check1.read()) and ('MOZ_BRANDING_DIRECTORY=browser/branding/dot' in check2.read())):
+if (('recurse_frontend' in check1.read()) and ('MOZ_APP_VENDOR="Dot HQ"' in check2.read())):
     print("✅ Patches have applied successfully.")
 else:
     sys.exit('❌ Patches failed to apply.')
