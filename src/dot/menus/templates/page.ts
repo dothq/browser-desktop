76eb3f64a8c9afc4adde01a12e4cb2f8f258e0e0
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-08-11 22:46:49 +0100

    📖 Add page menu template

---
+++ src/dot/menus/templates/page.ts
@@ -0,0 +1,26 @@
+import { AddBookmark } from "../components/add-bookmark";
+import { GoBack } from "../components/go-back";
+import { GoForward } from "../components/go-forward";
+import { InspectElement } from "../components/inspect-element";
+import { PrintPage } from "../components/print";
+import { Reload } from "../components/reload";
+import { SavePageAs } from "../components/save-as";
+import { ScreenshotPage } from "../components/screenshot";
+import { ________________ } from "../components/separator";
+import { ViewPageSource } from "../components/view-page-source";
+
+export class PageMenu {
+    public defaultLayout = [
+        GoBack,
+        GoForward,
+        Reload,
+        AddBookmark,
+        ________________,
+        SavePageAs,
+        PrintPage,
+        ScreenshotPage,
+        ________________,
+        ViewPageSource,
+        InspectElement
+    ]
+}
