7b47dd5d7c50ca76d3c5136bac6c707860024536
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-08-11 22:42:22 +0100

    🤝 Add truncate helper function

--- src/dot/menus/common.ts
+++ src/dot/menus/common.ts
@@ -3,3 +3,5 @@ import { MenuItem } from ".";
 export const separator: MenuItem = {
     type: "separator"
 }
+
+export const trunc = (input: string, max: number) => input.length > max ? `${input.substring(0, max)}…` : input;
