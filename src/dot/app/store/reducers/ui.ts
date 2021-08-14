55516a899dfa0e299928494e2a3ee0f8974623fe
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-08-14 22:19:19 +0100

    🔥 To remove: ui reducer

--- src/dot/app/store/reducers/ui.ts
+++ src/dot/app/store/reducers/ui.ts
@@ -1,17 +1,24 @@
-import { createReducer } from '@reduxjs/toolkit';
+import { AnyAction, createReducer } from '@reduxjs/toolkit';
 
 interface TabsState {
-
+    menuitems: {
+        id: string,
+        menu: any[],
+        iconPrefix?: string
+    } | null
 }
 
 const initialState: TabsState = {
-
+    menuitems: null
 }
 
 export const uiReducer = createReducer(
     initialState,
     {
-
+        UI_SET_MENU: (store, action: AnyAction) => {
+            store.menuitems = action.payload;
+            console.log(action.payload);
+        }
     }
 );
 
