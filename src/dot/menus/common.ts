5c130c3d34f2fbebe2f925912f342a2bcdf72e0d
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-08-14 22:15:44 +0100

    🔥 Remove common menu utils

--- src/dot/menus/common.ts
+++
@@ -1,7 +0,0 @@
-import { MenuItem } from ".";
-
-export const separator: MenuItem = {
-    type: "separator"
-}
-
-export const trunc = (input: string, max: number) => input.length > max ? `${input.substring(0, max)}…` : input;
