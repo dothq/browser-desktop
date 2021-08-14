0245d7901d4aae2b74351eccd054cbd0cb239b49
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-08-14 22:15:31 +0100

    🚧 Add WIP menu popper wrapper component

---
+++ src/dot/components/MenuPopper/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { dot } from "../../api";
+
+export const MenuPopper = ({ children, menu, ctx }: { children: any, menu: string, ctx?: any }) => {
+    const childRef = React.useRef<HTMLElement>();
+    const [open, setOpen] = React.useState(false);
+    const [id, setId] = React.useState("");
+
+    React.useEffect(() => {
+        if (
+            dot.menus.visibleMenu &&
+            dot.menus.visibleMenuId == id
+        ) {
+            // menu is open + it is the correct menu
+            setOpen(true);
+        } else {
+            setOpen(false);
+        }
+    }, [dot.menus.visibleMenu])
+
+    const onClick = () => {
+        if (!childRef.current) return;
+
+        const { x, y, width, height } = childRef.current.getBoundingClientRect();
+
+        const templated = dot.menus.create(
+            menu,
+            { x: x + width, y: y + height },
+            ctx || {}
+        );
+
+        if (templated) setId(templated.id);
+    }
+
+    return (
+        <>
+            {React.cloneElement(React.Children.only(children), {
+                ref: childRef,
+                onClick,
+                "data-menu-open": open ? true : undefined
+            })}
+        </>
+    )
+}
