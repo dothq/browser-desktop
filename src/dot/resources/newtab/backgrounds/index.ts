63d8a3da4de5794f7ab8c24c083b9862008b9c40
trickypr <trickypr@icloud.com>
2021-08-19 18:31:24 +1000

    🍱 Add more ntp images

--- src/dot/resources/newtab/backgrounds/index.ts
+++ src/dot/resources/newtab/backgrounds/index.ts
@@ -26,5 +26,40 @@ export const backgrounds = [
             name: "Jase Bloor",
             url: "https://unsplash.com/@jasebloor"
         }
+    },
+    {
+        id: "blur-city",
+        author: {
+            name: "Thong Vo",
+            url: "https://unsplash.com/@titi_wanderer"
+        }
+    },
+    {
+        id: "lake",
+        author: {
+            name: "Ales Krivec",
+            url: "https://unsplash.com/@aleskrivec"
+        }
+    },
+    {
+        id: "night-city",
+        author: {
+            name: "Riku Lu",
+            url: "https://unsplash.com/@riku"
+        }
+    },
+    {
+        id: "path",
+        author: {
+            name: "Beata Ratuszniak",
+            url: "https://unsplash.com/@beataratuszniak"
+        }
+    },
+    {
+        id: "road",
+        author: {
+            name: "Karsten Würth",
+            url: "https://unsplash.com/@karsten_wuerth"
+        }
     }
-]
+];
