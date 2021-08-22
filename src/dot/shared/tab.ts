8cde3196c2d18e1ac0b7787e3d9cf97e33a78bcf
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-08-22 22:08:57 +0100

    🐛 Fix the default favicon for the new tab page

--- src/dot/shared/tab.ts
+++ src/dot/shared/tab.ts
@@ -2,3 +2,7 @@ import { Services } from "../modules";
 
 export const NEW_TAB_URL = "about:newtab";
 export const NEW_TAB_URL_PARSED = Services.io.newURI(NEW_TAB_URL);
+
+export const hideFaviconFor = [
+    "about:newtab"
+]
