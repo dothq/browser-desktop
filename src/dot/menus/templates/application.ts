0f54ba0f0e405b00ddb86c32521723ffd3a0b059
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-08-22 22:09:46 +0100

    💄 Add seperator before Quit item in app menu

--- src/dot/menus/templates/application.ts
+++ src/dot/menus/templates/application.ts
@@ -38,6 +38,7 @@ export class AppMenu extends TemplatedMenu {
                 ________________,
                 Settings,
                 Help,
+                ________________,
                 Quit
             ]
         })
