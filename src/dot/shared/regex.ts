2fc54f66b37b969fa9fa0f64495a94ff2ddde7d5
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-08-25 22:22:10 +0100

    💬 Add URL matching regex

--- src/dot/shared/regex.ts
+++ src/dot/shared/regex.ts
@@ -14,3 +14,7 @@ export const COMMENT_REGEX = new RegExp(
 `.replace(/\s+/g, ""),
     "gm"
 );
+
+export const URL_WITH_PROTO_REGEX = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
+export const URL_NAKED_REGEX = /^(?:(?:(?:[a-zA-z\-]+)\:\/{1,3})?(?:[a-zA-Z0-9])(?:[a-zA-Z0-9\-\.]){1,61}(?:\.[a-zA-Z]{2,})+|\[(?:(?:(?:[a-fA-F0-9]){1,4})(?::(?:[a-fA-F0-9]){1,4}){7}|::1|::)\]|(?:(?:[0-9]{1,3})(?:\.[0-9]{1,3}){3})|localhost)(?:\:[0-9]{1,5})?$/;
+export const URL_CHROME_PATH_REGEX = /\/[a-zA-Z0-9]*\/content/;
