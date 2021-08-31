47a9285f317a03569b3ed1aee2fd7e3d3f82ec33
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-08-31 22:14:39 +0100

    💬 Add security tab to site identity dialog

---
+++ src/dot/core/site-identity/security.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Tab } from "../../models/Tab"
+
+export const SiteIdentitySecurityScreen = ({ tab }: { tab?: Tab }) => {
+    return (
+        <div>
+            security screen
+        </div>
+    )
+}
