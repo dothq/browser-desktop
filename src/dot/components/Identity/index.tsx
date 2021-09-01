4688aa54f0fc490edd8d6e54349b522891c0405a
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-09-01 22:00:46 +0100

    🏷️ Make identity type a string

--- src/dot/components/Identity/index.tsx
+++ src/dot/components/Identity/index.tsx
@@ -5,18 +5,11 @@ const labels: any = {
     "built-in": "Dot Browser"
 }
 
-export const Identity = ({ type, onClick, selected }: {
-    type: 'search' |
-    'info' |
-    'warning' |
-    'built-in' |
-    'http' |
-    'https' |
-    'https-unsecure' |
-    'extension' |
-    'file',
-    onClick?: any
-    selected?: boolean
+export const Identity = ({ type, onClick, selected, title }: {
+    type: string,
+    onClick?: any,
+    selected?: boolean,
+    title?: string
 }) => {
     return (
         <SearchbarButton
