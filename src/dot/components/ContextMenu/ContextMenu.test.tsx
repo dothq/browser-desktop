68fc1e8916b69ec031f1d1e934e9b133726680bd
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-09-12 18:18:33 +0100

    🔥 Remove ContextMenu component as Menu API handles that now

--- src/dot/components/ContextMenu/ContextMenu.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-import React from 'react'
-import { ContextMenu } from '.'
-
-describe('UI Context Menu', () => {
-    test('Render', async () => {
-        render(<ContextMenu id="test"><span>child</span></ContextMenu>)
-    })
-})
