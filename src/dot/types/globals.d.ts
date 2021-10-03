cd91788cd645c307e43717cfeaebdd3bb9f3dd58
trickypr <trickypr@icloud.com>
2021-10-03 12:25:40 +1100

    🏷️ Use Dot type for window.dot

--- src/dot/types/globals.d.ts
+++ src/dot/types/globals.d.ts
@@ -1,22 +1,25 @@
+import type { Dot } from '../api';
 
 declare module '*.ftl'
 
-interface Document {
-    createXULElement: (element: string) => HTMLElement;
-}
+declare global {
+    interface Document {
+        createXULElement: (element: string) => HTMLElement;
+    }
 
-interface Window {
-    docShell: any;
-    XULBrowserWindow: any;
-    windowState: number;
-    STATE_MAXIMIZED: number;
-    STATE_MINIMIZED: number;
-    STATE_NORMAL: number;
-    STATE_FULLSCREEN: number;
-    content: any;
-    openDialog: any;
-    windowRoot: {
-        ownerGlobal: Window
-    };
-    dot: any
+    interface Window {
+        docShell: any;
+        XULBrowserWindow: any;
+        windowState: number;
+        STATE_MAXIMIZED: number;
+        STATE_MINIMIZED: number;
+        STATE_NORMAL: number;
+        STATE_FULLSCREEN: number;
+        content: any;
+        openDialog: any;
+        windowRoot: {
+            ownerGlobal: Window
+        };
+        dot: Dot
+    }
 }
