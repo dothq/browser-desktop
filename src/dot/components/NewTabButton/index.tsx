eb47db07eb69fb18f04a22190aed7bacf8698b0b
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-12-05 18:23:34 +0000

    ✏️ Add an ID to NewTabButton

--- src/dot/components/NewTabButton/index.tsx
+++ src/dot/components/NewTabButton/index.tsx
@@ -2,9 +2,12 @@ import { observer } from "mobx-react";
 import React from "react";
 import { ToolbarButton } from "../ToolbarButton";
 
-export const NewTabButton = observer(() => (
-    <ToolbarButton
-        image={"chrome://dot/content/skin/icons/add.svg"}
-        command={"Browser:NewTab"}
-    />
-));
+export const NewTabButton = observer(() => {
+    return (
+        <ToolbarButton
+            id={"new-tab-button"}
+            image={"chrome://dot/content/skin/icons/add.svg"}
+            command={"Browser:NewTab"}
+        />
+    )
+});
