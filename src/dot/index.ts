d4fb0f39d0f72939c9526a1dfa95f553f9cbf19d
EnderDev <42723993+EnderDev@users.noreply.github.com>
2022-01-16 22:04:24 +0000

    🔥 Remove UI logic from entry

--- src/dot/index.ts
+++ src/dot/index.ts
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import BrowserCustomisableUI from "browser/customisable";
 import BrowserExtensions from "browser/extensions";
 import BrowserInit from "browser/init";
 import BrowserMotion from "browser/motion";
@@ -9,20 +10,10 @@ import BrowserPreferences from "browser/preferences";
 import BrowserStorage from "browser/storage";
 import BrowserTabs from "browser/tabs";
 import BrowserThemes from "browser/themes";
-import BrowserTitlebar from "browser/titlebar";
 import BrowserUtilities from "browser/utilities";
 import BrowserWindow from "browser/window";
 import "l10n";
 import { BrowserToolboxLauncher, Ci } from "mozilla";
-import {
-    aside,
-    createRef,
-    div,
-    getDOMNode,
-    RefObject,
-    render,
-    _
-} from "oikia";
 import "themes/browser.scss";
 import "themes/tab.scss";
 import "themes/titlebar.scss";
@@ -31,13 +22,13 @@ import { Events } from "./events";
 import { exportPublic } from "./shared/globals";
 
 export class Browser extends Events {
+    public customisable = new BrowserCustomisableUI(this);
     public extensions = new BrowserExtensions(this);
     public init = new BrowserInit(this);
     public motion = new BrowserMotion(this);
     public preferences = new BrowserPreferences(this);
     public storage = new BrowserStorage(this);
     public tabs = new BrowserTabs(this);
-    public titlebar = new BrowserTitlebar(this);
     public themes = new BrowserThemes(this);
     public utilities = new BrowserUtilities(this);
     public window = new BrowserWindow(this);
@@ -54,26 +45,6 @@ export class Browser extends Events {
     public isFission = window.docShell.QueryInterface(Ci.nsILoadContext)
         .useRemoteSubframes;
 
-    /**
-     * HTML reference to the Titlebar
-    */
-    public titlebarRef: RefObject<HTMLDivElement> | undefined;
-
-    /**
-     * HTML reference to the Toolbar
-    */
-    public toolbarRef: RefObject<HTMLDivElement> | undefined;
-
-    /**
-     * HTML reference to the Bookmarks Bar
-    */
-    public bookmarksBarRef: RefObject<HTMLDivElement> | undefined;
-
-    /**
-     * HTML reference to the Browser Contents
-    */
-    public contentRef: RefObject<HTMLDivElement> | undefined;
-
     public constructor() {
         super();
 
@@ -81,11 +52,11 @@ export class Browser extends Events {
     }
 
     public async load() {
-        // Wait for the storage service to initialise
-        await this.storage.connect();
-
-        const component = this.render();
-        render(component, getDOMNode("#browser"));
+        /*
+            Initialise vital services.
+        */
+        await this.storage.init();
+        await this.customisable.init();
         
         // Unlock the browser error handling.
         // All errors can be treated as warnings.
@@ -95,36 +66,7 @@ export class Browser extends Events {
         BrowserToolboxLauncher.init();
 
         this.themes.load();
-        this.tabs.createInitialTab();
-    }
-
-    public render() {
-        this.titlebarRef = createRef<HTMLDivElement>();
-        this.toolbarRef = createRef<HTMLDivElement>();
-        this.bookmarksBarRef = createRef<HTMLDivElement>();
-        this.contentRef = createRef<HTMLDivElement>();
-
-        const Titlebar = this.titlebar.render(this.titlebarRef);
-
-        return _(
-            Titlebar,
-            div({
-                id: "browser-toolbar",
-                ref: this.toolbarRef
-            }),
-            div({
-                id: "browser-bookmarkbar",
-                ref: this.bookmarksBarRef
-            }),
-            div(
-                {
-                    id: "browser-content",
-                    ref: this.contentRef
-                },
-                aside({ id: "browser-content-sidebar" }),
-                div({ id: "browser-content-tabbox" })
-            )
-        );
+        // this.tabs.createInitialTab();
     }
 }
 
