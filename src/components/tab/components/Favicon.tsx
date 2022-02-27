bcd18b2003269a46cda55c243de4abff931ada87
EnderDev <42723993+EnderDev@users.noreply.github.com>
2022-02-27 23:16:35 +0000

    💥 Add Tab component

---
+++ src/components/tab/components/Favicon.tsx
@@ -0,0 +1,22 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Icon from "browser/components/common/icon";
+import React, { PureComponent } from "react";
+import { StyledFavicon } from "./Favicon.style";
+
+class TabFavicon extends PureComponent {
+	public render() {
+		return (
+			<StyledFavicon>
+				<Icon
+					size={16}
+					icon={"https://permission.site/favicon.ico"}
+				/>
+			</StyledFavicon>
+		);
+	}
+}
+
+export default TabFavicon;
