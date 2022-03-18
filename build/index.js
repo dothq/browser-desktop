ddc24e9232573acb6092836fae8d4193800c7ccf
EnderDev <42723993+EnderDev@users.noreply.github.com>
2022-03-18 23:29:58 +0000

    🍉 WIP: E2E testing

--- build/index.js
+++ build/index.js
@@ -5,114 +5,97 @@
 const { fork } = require("child_process");
 const { build } = require("esbuild");
 const execa = require("execa");
-const {
-    writeFileSync,
-    readFileSync,
-    existsSync
-} = require("fs");
+const { writeFileSync, readFileSync, existsSync } = require("fs");
 const { lookpath } = require("lookpath");
 const { resolve } = require("path");
 const { createHash } = require("crypto");
 const { ensureDirSync } = require("fs-extra");
 
 const useNodePkgMgr = () => {
-    const yarnExists = lookpath("yarn");
+	const yarnExists = lookpath("yarn");
 
-    return yarnExists ? "yarn" : "npm";
+	return yarnExists ? "yarn" : "npm";
 };
 
 const genHashOfPackageJson = () => {
-    const pkgLock = createHash("sha1");
-    pkgLock.update(
-        readFileSync(
-            resolve(process.cwd(), "package.json"),
-            "utf-8"
-        )
-    );
+	const pkgLock = createHash("sha1");
+	pkgLock.update(
+		readFileSync(resolve(process.cwd(), "package.json"), "utf-8")
+	);
 
-    return pkgLock.digest("hex");
+	return pkgLock.digest("hex");
 };
 
 const canInstallDeps = () => {
-    const lockPath = resolve(
-        process.cwd(),
-        ".melon",
-        "LOCK"
-    );
+	const lockPath = resolve(process.cwd(), ".melon", "LOCK");
 
-    if (existsSync(lockPath)) {
-        const lockHash = readFileSync(lockPath, "utf-8");
-        const currentHash = genHashOfPackageJson();
+	if (existsSync(lockPath)) {
+		const lockHash = readFileSync(lockPath, "utf-8");
+		const currentHash = genHashOfPackageJson();
 
-        if (lockHash == currentHash) {
-            return false;
-        } else {
-            writeFileSync(lockPath, currentHash);
-            return true;
-        }
-    } else {
-        writeFileSync(lockPath, genHashOfPackageJson());
-        return true;
-    }
+		if (lockHash == currentHash) {
+			return false;
+		} else {
+			writeFileSync(lockPath, currentHash);
+			return true;
+		}
+	} else {
+		writeFileSync(lockPath, genHashOfPackageJson());
+		return true;
+	}
 };
 
 const installDependencies = async () => {
-    return new Promise((resolve) => {
-        if (!canInstallDeps()) return resolve(true);
+	return new Promise((resolve) => {
+		if (!canInstallDeps()) return resolve(true);
 
-        const mgr = useNodePkgMgr();
+		const mgr = useNodePkgMgr();
 
-        const installProcess = execa(mgr, ["install"]);
-        installProcess.stdout.pipe(process.stdout);
+		const installProcess = execa(mgr, ["install"]);
+		installProcess.stdout.pipe(process.stdout);
 
-        installProcess.on("exit", (code) => {
-            resolve(code == 0 ? true : false);
-        });
-    });
+		installProcess.on("exit", (code) => {
+			resolve(code == 0 ? true : false);
+		});
+	});
 };
 
 const main = async () => {
-    ensureDirSync(resolve(process.cwd(), ".melon"));
+	ensureDirSync(resolve(process.cwd(), ".melon"));
 
-    let timeout = setTimeout(() => {
-        console.log(
-            "Melon is taking longer than expected. It is still working so give it some time before killing."
-        );
-    }, 5000);
+	let timeout = setTimeout(() => {
+		console.log(
+			"Melon is taking longer than expected. It is still working so give it some time before killing."
+		);
+	}, 5000);
 
-    process.env["FORCE_COLOR"] = "true";
+	process.env["FORCE_COLOR"] = "true";
 
-    await installDependencies();
+	await installDependencies();
 
-    build({
-        entryPoints: [
-            resolve(process.cwd(), "build", "index.ts")
-        ],
-        bundle: true,
-        target: "node16",
-        platform: "node",
-        sourcemap: true,
-        external: ["bsdiff-node"],
-        outdir: resolve(process.cwd(), ".melon")
-    })
-        .then((_) => {
-            const proc = fork(
-                resolve(
-                    process.cwd(),
-                    ".melon",
-                    "index.js"
-                ),
-                process.argv.splice(2),
-                { stdio: "inherit" }
-            );
+	build({
+		entryPoints: [resolve(process.cwd(), "build", "index.ts")],
+		bundle: true,
+		target: "node16",
+		platform: "node",
+		sourcemap: true,
+		external: ["bsdiff-node", "esbuild"],
+		outdir: resolve(process.cwd(), ".melon"),
+	})
+		.then((_) => {
+			const proc = fork(
+				resolve(process.cwd(), ".melon", "index.js"),
+				process.argv.splice(2),
+				{ stdio: "inherit" }
+			);
 
-            proc.on("exit", (code) => {
-                process.exit(code);
-            });
-        })
-        .finally(() => {
-            clearTimeout(timeout);
-        });
+			proc.on("exit", (code) => {
+				process.exit(code);
+			});
+		})
+		.finally(() => {
+			clearTimeout(timeout);
+		});
 };
 
 main();
