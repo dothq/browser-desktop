c76d44b7ed0bf302dba7a18852b2538f910d8687
EnderDev <kieran@dothq.org>
2022-09-03 17:46:58 +0100

    🔥 Remove debugging preferences

--- app/profile/dot.js
+++ app/profile/dot.js
@@ -6,13 +6,6 @@
 
 pref("toolkit.defaultChromeURI", "chrome://dot/content/browser.html");
 
-/* debugging prefs, disable these before you deploy your application! */
-pref("browser.dom.window.dump.enabled", true);
-pref("javascript.options.showInConsole", true);
-pref("javascript.options.strict", true);
-pref("nglayout.debug.disable_xul_cache", true);
-pref("nglayout.debug.disable_xul_fastload", true);
-
 pref("devtools.debugger.server-port", 40291);
 
 // Whether to show the dialogs opened at the content level, such as
