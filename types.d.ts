b5e42f8bf8e8ed5d90cda4e1feb8fac3ec2642d1
EnderDev <kieran@dothq.org>
2022-09-06 21:30:37 +0100

    🏷️ Create exclusive types for Screen

--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,11 @@
-declare module "resource://*";
-declare module "chrome://*";
+import "gecko-types";
 
-declare module "resource://app/modules/DevToolsServer.jsm";
+declare module "resource://app/modules/DevToolsServer.jsm" {}
+
+declare global {
+	/* Only available in secure contexts */
+	interface Screen {
+		availTop: number;
+		availLeft: number;
+	}
+}
