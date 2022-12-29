126682a4f16fcab8f08f7b3ededb38033e6e0bb5
EnderDev <kieran@dothq.org>
2022-12-29 18:15:07 +0000

    💄 Add StatusPanel component to browser-element

---
+++ components/browser-element/content/StatusPanel.tsx
@@ -0,0 +1,31 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+class StatusPanelLabel extends MozHTMLElement {
+	public get value() {
+		return this.textContent;
+	}
+
+	public set value(newValue: string) {
+		this.textContent = newValue;
+	}
+
+	public constructor() {
+		super();
+	}
+}
+
+customElements.define("browser-statuspanel-label", StatusPanelLabel);
+
+export default class StatusPanel extends MozHTMLElement {
+	public constructor() {
+		super();
+
+		const markup = <browser-statuspanel-label id="statuspanel-label" />;
+
+		this.appendChild(markup);
+	}
+}
+
+customElements.define("browser-statuspanel", StatusPanel);
