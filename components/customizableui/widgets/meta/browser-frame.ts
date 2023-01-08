f977727ef1f84f14b844006c5b50b39a718c2808
EnderDev <kieran@dothq.org>
2023-01-08 23:55:17 +0000

    ✨ Add browser-frame widget

---
+++ components/customizableui/widgets/meta/browser-frame.ts
@@ -0,0 +1,30 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { CustomizableUIWidgetDisplay } from "../../CustomizableUIWidgets.js";
+import Widget from "../common/index.js";
+import { applyConfig } from "../index.js";
+
+class BrowserFrameWidget extends Widget {
+	public render() {
+		return document.querySelector("#browser");
+	}
+
+	public constructor() {
+		const props = {
+			id: "browser-frame",
+			visible: true,
+			display: CustomizableUIWidgetDisplay.IconsBesideText,
+			configurableProps: {}
+		};
+
+		super(props);
+
+		applyConfig(this, props);
+	}
+}
+
+customElements.define("widget-browser-frame", BrowserFrameWidget);
+
+export default BrowserFrameWidget;
