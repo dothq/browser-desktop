59030cfa01dad3381f9487eb9691efb5331e3472
EnderDev <kieran@dothq.org>
2023-02-12 22:22:59 +0000

    🧱 Use pure JS for development helpers in case of compiler issues

--- base/content/browser-development-helpers.js
+++ base/content/browser-development-helpers.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const { DevToolsServer } = ChromeUtils.importESModule<any>(
+const { DevToolsServer } = ChromeUtils.importESModule(
 	"resource://app/modules/DevToolsServer.sys.mjs"
 );
 
