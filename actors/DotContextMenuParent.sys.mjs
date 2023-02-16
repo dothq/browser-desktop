e73776cc799192f2717feead94a429a4fcd19f07
EnderDev <kieran@dothq.org>
2023-02-16 15:30:45 +0000

    🐛 Remove remains of JSM from ES module

--- actors/DotContextMenuParent.sys.mjs
+++ actors/DotContextMenuParent.sys.mjs
@@ -2,9 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, you can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const EXPORTED_SYMBOLS = ["ContextMenuParent"];
-
-class ContextMenuParent extends JSWindowActorParent {
+export class ContextMenuParent extends JSWindowActorParent {
 	receiveMessage(message) {
 		if (message.name !== "contextmenu") return;
 
