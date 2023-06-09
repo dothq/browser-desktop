1859fe2575833578a6757beba680af27158c9862
EnderDev <kieran@dothq.org>
2023-06-09 14:11:32 +0100

    🚚 Rename new tab page to startpage internally

--- components/panel/Panel.tsx
+++ components/panel/Panel.tsx
@@ -2,18 +2,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-export default class Panel extends MozHTMLElement {
-	public constructor() {
-		super();
+customElements.define(
+	"browser-panel",
+	class Panel extends MozHTMLElement {
+		public constructor() {
+			super();
 
-		const markup = (
-			<div class="panel-container">
-				<h4>Testing panel</h4>
-			</div>
-		);
+			const markup = (
+				<div class="panel-container">
+					<h4>Testing panel</h4>
+				</div>
+			);
 
-		this.appendChild(markup);
+			this.appendChild(markup);
+		}
 	}
-}
-
-customElements.define("browser-panel", Panel);
+);
