77ad22e77167a7b3f0b051356f07ad335ee24af6
EnderDev <kieran@dothq.org>
2023-06-09 13:53:50 +0100

    🏷️ Add type for DevToolsSocketStatus module

---
+++ third_party/dothq/gecko-types/lib/DevToolsSocketStatus.d.ts
@@ -0,0 +1,16 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+export interface DevToolsSocketStatus {
+	hasSocketOpened(options: {
+		excludeBrowserToolboxSockets?: boolean;
+	}): boolean;
+
+	notifySocketOpened(options: {
+		fromBrowserToolbox?: boolean;
+	}): void;
+	notifySocketClosed(options: {
+		fromBrowserToolbox?: boolean;
+	}): void;
+}
