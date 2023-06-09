f1519b8e9087df6ea3aa6c3f289b8580cdfb3359
EnderDev <kieran@dothq.org>
2023-06-09 13:56:14 +0100

    🚧 Add cheap types to be revised in future

--- third_party/dothq/gecko-types/lib/InspectorUtils.d.ts
+++ third_party/dothq/gecko-types/lib/InspectorUtils.d.ts
@@ -2,9 +2,4 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-export interface XULElement extends HTMLElement {
-	new (): XULElement;
-	prototype: XULElement;
-
-	isInstance: (el: XULElement) => boolean;
-}
+export type InspectorUtils = any;
