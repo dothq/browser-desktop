857f1eab75b3a7f4e9cfde601c770718e6cb4eca
EnderDev <kieran@dothq.org>
2023-06-20 23:52:49 +0100

    💞 Add proxy compatibility layer for tabs list

--- base/content/browser-compat.ts
+++ base/content/browser-compat.ts
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { ChromeBrowser } from "third_party/dothq/gecko-types/lib";
-
 /**
  * browser-compat is used as a compatibility layer to translate Dot APIs to the original FF/Gecko APIs
  * 
@@ -13,15 +11,56 @@ import { ChromeBrowser } from "third_party/dothq/gecko-types/lib";
  * For this reason, it's easier for us to create a compatibility layer between our APIs and the Mozilla APIs,
  * to avoid breaking these important scripts.
  * 
- * @deprecated You shouldn't use this in Dot code directly! This is purely intended for use by existing Mozilla modules and scripts to maintain compatibility.
+ * **You shouldn't use this in Dot code directly! This is purely intended to preserve the functionality 
+ * of existing Mozilla modules and scripts, to maintain compatibility.**
  */
+
+function defineGetter(object: object, name: string, getter: () => any, options?: PropertyDescriptor) {
+    return Object.defineProperty(object, name, {
+        ...(options || {}),
+        get: getter
+    });
+} 
+
+function defineSetter(object: object, name: string, setter: (v: any) => void, options?: PropertyDescriptor) {
+    return Object.defineProperty(object, name, {
+        ...(options || {}),
+        set: setter
+    });
+} 
+
+function proxyBrowserTab(tab: BrowserTab) {
+    defineGetter(tab, "linkedBrowser", () => {
+        if ((tab.webContents as ChromeBrowser).browserId) {
+            return tab.webContents;
+        }
+
+        // When our webContents isn't a browser, just create a new blank browser
+        return document.createXULElement("browser");
+    });
+
+    return tab;
+}
+
+function proxyBrowserTabsList(list: any[]) {
+    return Array.from(list).map(proxyBrowserTab);
+}
+
 var gBrowser = {
+    get tabs() {
+        return proxyBrowserTabsList(gDot.tabs.list);
+    },
+
     get currentURI() {
-        return Services.io.newURI("about:blank")
+        return this.selectedBrowser.currentURI;
+    },
+
+    get selectedTab() {
+        return gDot.tabs.selectedTab;
     },
 
     get selectedBrowser() { 
-        return gDot.tabs.selectedBrowser;
+        return gDot.tabs.selectedTab.webContents;
     },
 
     get ownerGlobal() {
@@ -33,7 +72,7 @@ var gBrowser = {
     },
 
     getTabForBrowser(browser: ChromeBrowser) {
-        return gDot.tabs.getTabForBrowser(browser);
+        return gDot.tabs.getTabForWebContents(browser);
     }
 }
 
