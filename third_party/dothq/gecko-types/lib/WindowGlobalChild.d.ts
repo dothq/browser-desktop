3440c683d8e1a1f55f9d83fa33189a7cf6ce551b
EnderDev <kieran@dothq.org>
2023-07-03 22:32:37 +0100

    🏷️ Allow WindowGlobalChild instance to be reused

--- third_party/dothq/gecko-types/lib/WindowGlobalChild.d.ts
+++ third_party/dothq/gecko-types/lib/WindowGlobalChild.d.ts
@@ -6,37 +6,41 @@ import { BrowsingContext } from "./BrowsingContext";
 import { JSWindowActorChild } from "./JSWindowActorChild";
 import { WindowGlobalParent } from "./WindowGlobalParent";
 
+export interface WindowGlobalChildInstance {
+	readonly isClosed: boolean;
+	readonly isInProcess: boolean;
+	readonly browsingContext: BrowsingContext;
+	readonly windowContext: any;
+
+	readonly isCurrentGlobal: boolean;
+
+	readonly innerWindowId: number;
+	readonly outerWindowId: number;
+	readonly contentParentId: number;
+
+	// A WindowGlobalChild is the root in its process if it has no parent, or its
+	// embedder is in a different process.
+	readonly isProcessRoot: boolean;
+
+	// Is this WindowGlobalChild same-origin with `window.top`?
+	readonly sameOriginWithTop: boolean;
+
+	readonly parentActor?: WindowGlobalParent; // in-process only
+
+	/**
+	 * Get or create the JSWindowActor with the given name.
+	 *
+	 * See WindowActorOptions from JSWindowActor.webidl for details on how to
+	 * customize actor creation.
+	 */
+	getActor(name: string): JSWindowActorChild;
+	getExistingActor(name: string): JSWindowActorChild | null;
+}
+
 export interface WindowGlobalChild {
-    new(): {
-        readonly isClosed: boolean;
-        readonly isInProcess: boolean;
-        readonly browsingContext: BrowsingContext;
-        readonly windowContext: any;
+	new (): WindowGlobalChildInstance;
 
-        readonly isCurrentGlobal: boolean;
-
-        readonly innerWindowId: number;
-        readonly outerWindowId: number;
-        readonly contentParentId: number;
-
-        // A WindowGlobalChild is the root in its process if it has no parent, or its
-        // embedder is in a different process.
-        readonly isProcessRoot: boolean;
-
-        // Is this WindowGlobalChild same-origin with `window.top`?
-        readonly sameOriginWithTop: boolean;
-
-        readonly parentActor?: WindowGlobalParent; // in-process only
-
-        /**
-         * Get or create the JSWindowActor with the given name.
-         *
-         * See WindowActorOptions from JSWindowActor.webidl for details on how to
-         * customize actor creation.
-         */
-        getActor(name: string): JSWindowActorChild;
-        getExistingActor(name: string): JSWindowActorChild | null;
-    }
-
-    getByInnerWindowId(innerWindowId: number): WindowGlobalChild | null;
+	getByInnerWindowId(
+		innerWindowId: number
+	): WindowGlobalChild | null;
 }
