7dbd4b13f0f0dec9ac10df76545b72e0af01c750
EnderDev <kieran@dothq.org>
2023-07-10 19:58:13 +0100

    💄 Register dev-debug-panel widget

--- base/content/browser-elements.ts
+++ base/content/browser-elements.ts
@@ -2,7 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const elements = {
+var { AppConstants } = ChromeUtils.importESModule(
+    "resource://gre/modules/AppConstants.sys.mjs"
+);
+
+let elements = {
     "browser-tab": "chrome://dot/content/widgets/browser-tab.js",
     "browser-panel": "chrome://dot/content/widgets/browser-panel.js",
     "browser-frame": "chrome://dot/content/widgets/browser-frame.js",
@@ -10,6 +14,15 @@ const elements = {
     "browser-modals": "chrome://dot/content/widgets/browser-modals.js",
     "browser-content-popup": "chrome://dot/content/widgets/browser-content-popup.js",
     "browser-window-controls": "chrome://dot/content/widgets/browser-window-controls.js",
+}
+
+// Define developer-only elements
+// Locked behind MOZILLA_OFICIAL so they don't end up in release builds
+if (!AppConstants.MOZILLA_OFFICIAL) {
+    elements = {
+        ...elements,
+        "dev-debug-panel": "chrome://dot/content/widgets/dev-debug-panel.js"
+    } as any;
 }
 
 for (const [element, chromeURI] of Object.entries(elements)) {
