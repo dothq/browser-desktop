9e204b351051edfe690f1f50bf7a3541fd0589c5
EnderDev <kieran@dothq.org>
2023-08-15 15:26:51 +0100

    💄 Add browser-icon component for rendering theme icons

---
+++ components/widgets/content/browser-icon.js
@@ -0,0 +1,59 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+class BrowserIcon extends MozHTMLElement {
+    static get observedAttributes() {
+        return [
+            "name",
+            "size"
+        ];
+    }
+
+    constructor() {
+        super();
+    }
+
+    get name() {
+        return this.getAttribute("name");
+    }
+
+    set name(newName) {
+        this.setAttribute("name", newName);
+    }
+
+    get size() {
+        return this.getAttribute("size");
+    }
+
+    set size(newSize) {
+        this.setAttribute("size", newSize);
+        this.style.setProperty("--size", newSize);
+    }
+
+    connectedCallback() {
+        if (this.delayConnectedCallback()) return;
+    }
+
+    disconnectedCallback() {
+        if (this.delayConnectedCallback()) return;
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (!this.isConnectedAndReady) return;
+
+        switch (name) {
+            case "src":
+                if (newValue !== oldValue) {
+                    this.src = newValue;
+                }
+                break;
+            case "size":
+                if (newValue !== oldValue) {
+                    this.size = newValue;
+                }
+        }
+    }
+}
+
+customElements.define("browser-icon", BrowserIcon);
