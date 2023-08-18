89b903660c740639b0ae386f45486dfe54347b44
EnderDev <kieran@dothq.org>
2023-08-18 17:12:19 +0100

    🚚 Move debug panel popout init to development helpers script

--- base/content/browser-development-helpers.js
+++ base/content/browser-development-helpers.js
@@ -3,14 +3,24 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const { DevToolsServer } = ChromeUtils.importESModule(
-	"resource://app/modules/DevToolsServer.sys.mjs"
+    "resource://app/modules/DevToolsServer.sys.mjs"
 );
 
 const DevelopmentHelpers = {
-	init() {
-		const devtools = DevToolsServer.get();
-		devtools.start();
-	}
+    init() {
+        const devtools = DevToolsServer.get();
+        devtools.start();
+
+        if (!Services.wm.getMostRecentWindow("Dot:DevDebugPopout")) {
+            Services.ww.openWindow(
+                null,
+                "chrome://dot/content/dev-debug-popout.xhtml",
+                "_blank",
+                "chrome,alwaysontop,dialog",
+                null
+            );
+        }
+    }
 };
 
 DevelopmentHelpers.init();
