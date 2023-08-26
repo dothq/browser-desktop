3c96731e75fb41e58a1c036204961d3349b8385d
EnderDev <kieran@dothq.org>
2023-08-26 15:10:49 +0100

    🚚 Move to using LWT in debug popout

--- components/dev/content/dev-debug-panel.js
+++ components/dev/content/dev-debug-panel.js
@@ -2,13 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var { AddonManager } = ChromeUtils.importESModule(
-    "resource://gre/modules/AddonManager.sys.mjs"
-);
+var { AddonManager } = ChromeUtils.importESModule("resource://gre/modules/AddonManager.sys.mjs");
 
-var { AppConstants } = ChromeUtils.importESModule(
-    "resource://gre/modules/AppConstants.sys.mjs"
-);
+var { AppConstants } = ChromeUtils.importESModule("resource://gre/modules/AppConstants.sys.mjs");
 
 var { DotAppConstants } = ChromeUtils.importESModule(
     "resource://gre/modules/DotAppConstants.sys.mjs"
@@ -16,26 +12,26 @@ var { DotAppConstants } = ChromeUtils.importESModule(
 
 /**
  * Utility function to convert bytes to a human-readable format
- * @param {number} bytes 
- * @param {number} decimals 
+ * @param {number} bytes
+ * @param {number} decimals
  * @returns {string}
  */
 function formatBytes(bytes, decimals = 2, k = 1024) {
-    if (!+bytes) return '0 Bytes'
+    if (!+bytes) return "0 Bytes";
 
-    const dm = decimals < 0 ? 0 : decimals
-    const sizes = ['Bytes', 'kB', 'MB', 'GB', 'TB']
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ["Bytes", "kB", "MB", "GB", "TB"];
 
-    const i = Math.floor(Math.log(bytes) / Math.log(k))
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
+    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
 }
 
 /**
  * Utility function to work out percentage difference between a value and its maximum
- * @param {number} val 
- * @param number max 
- * @returns 
+ * @param {number} val
+ * @param number max
+ * @returns
  */
 function perDiff(val, max) {
     return ((1.0 - (max - val) / max) * 100).toFixed(2);
@@ -49,25 +45,23 @@ class DeveloperDebugPanel extends MozHTMLElement {
     elements = {
         app_info: html("span"),
         proc_info: html("div"),
-        active_theme: html("div", { class: "dev-active-theme" }),
         user_agent: html("span"),
 
         graph: /** @type {DeveloperDebugGraph} */ (html("dev-debug-graph")),
-    }
+
+        active_theme: /** @type {HTMLSelectElement} */ (
+            html("select", { class: "dev-active-theme" })
+        )
+    };
 
     resourceUsageInt = null;
 
     onAddonEnabled(addon) {
         if (!addon || addon.type != "theme") return;
 
-        const icon = /** @type {HTMLImageElement} */ (this.elements.active_theme
-            .querySelector(".dev-active-theme-icon")
-        );
-
-        this.elements.active_theme.querySelector(".dev-active-theme-id").textContent = addon.id;
-
-        icon.hidden = !(addon.iconURL && addon.iconURL.length);
-        icon.src = addon.iconURL || "";
+        this.renderThemes().then((_) => {
+            this.elements.active_theme.value = addon.id;
+        });
     }
 
     async calculateResourceUsage() {
@@ -82,7 +76,8 @@ class DeveloperDebugPanel extends MozHTMLElement {
         ];
 
         if (procInfo.memory >= Math.max(...(this.elements.graph.points.default || []))) {
-            this.elements.graph.max = Math.ceil((procInfo.memory + 50000000 /* 50mb */) / 50000000) * 50000000;
+            this.elements.graph.max =
+                Math.ceil((procInfo.memory + 50000000) /* 50mb */ / 50000000) * 50000000;
         }
         this.elements.graph.addPoint(procInfo.memory);
 
@@ -94,13 +89,25 @@ class DeveloperDebugPanel extends MozHTMLElement {
 
                 const groupColour = this.elements.graph.pointGroupColours[child.pid.toString()];
 
-                const groupDot = /** @type {HTMLSpanElement} */ (html("div", { class: "dev-graph-group-dot" }));
+                const groupDot = /** @type {HTMLSpanElement} */ (
+                    html("div", { class: "dev-graph-group-dot" })
+                );
                 groupDot.style.setProperty("--color", groupColour);
 
-                data.push(html("div", { class: "dev-graph-group" },
-                    groupDot,
-                    html("span", {}, `${child.type} (id=${child.childID} pid=${child.pid}, mem=${formatBytes(child.memory)}, thds=${child.threads.length}, wins=${child.windows.length})`)
-                ))
+                data.push(
+                    html(
+                        "div",
+                        { class: "dev-graph-group" },
+                        groupDot,
+                        html(
+                            "span",
+                            {},
+                            `${child.type} (id=${child.childID} pid=${child.pid}, mem=${formatBytes(
+                                child.memory
+                            )}, thds=${child.threads.length}, wins=${child.windows.length})`
+                        )
+                    )
+                );
             }
         }
 
@@ -108,14 +115,22 @@ class DeveloperDebugPanel extends MozHTMLElement {
         this.elements.proc_info.append(...data);
     }
 
-    async init() {
-        const activeTheme = (await AddonManager.getAddonsByTypes(["theme"]))
-            .find(ext => ext.isActive);
+    async renderThemes() {
+        const allThemes = await AddonManager.getAddonsByTypes(["theme"]);
 
-        this.elements.active_theme.append(
-            "active_theme_id =",
-            html("img", { class: "dev-active-theme-icon" }),
-            html("span", { class: "dev-active-theme-id" })
+        // Clear children
+        this.elements.active_theme.replaceChildren();
+
+        for (const theme of allThemes.sort((a, b) => a.id.localeCompare(b.id))) {
+            const option = html("option", { value: theme.id }, `${theme.name} (${theme.id})`);
+
+            this.elements.active_theme.appendChild(option);
+        }
+    }
+
+    async init() {
+        const activeTheme = (await AddonManager.getAddonsByTypes(["theme"])).find(
+            (ext) => ext.isActive
         );
 
         AddonManager.addAddonListener({
@@ -133,19 +148,31 @@ class DeveloperDebugPanel extends MozHTMLElement {
         dotVersion.textContent = `Dot Browser v${DotAppConstants.DOT_APP_VERSION} (${AppConstants.MOZ_BUILDID})`;
 
         this.elements.app_info.append(
-            html("div", { class: "dev-branding-lockup" },
+            html(
+                "div",
+                { class: "dev-branding-lockup" },
                 html("img", { src: "chrome://branding/content/icon32.png" }),
-                html("img", { src: "chrome://branding/content/about-wordmark.svg", height: "48" }),
+                html("img", { src: "chrome://branding/content/about-wordmark.svg", height: "48" })
             ),
             html("br"),
             dotVersion,
             html("br"),
             `Firefox v${AppConstants.MOZ_APP_VERSION}`
-        )
+        );
 
-        this.elements.user_agent.textContent = `user_agent = ${Cc["@mozilla.org/network/protocol;1?name=http"].getService(
-            Ci.nsIHttpProtocolHandler
-        ).userAgent}`;
+        this.elements.user_agent.textContent = `user_agent = ${Cc["@mozilla.org/network/protocol;1?name=http"].getService(Ci.nsIHttpProtocolHandler)
+            .userAgent
+            }`;
+
+        this.elements.active_theme.addEventListener("change", async (event) => {
+            const { value } = /** @type {HTMLSelectElement} */ (event.target);
+
+            const addon = await AddonManager.getAddonByID(value);
+
+            if (addon) {
+                addon.enable();
+            }
+        });
     }
 
     insertStylesheet() {
@@ -154,10 +181,7 @@ class DeveloperDebugPanel extends MozHTMLElement {
             `href="chrome://dot/content/widgets/dev-debug-panel.css" type="text/css"`
         );
 
-        document.insertBefore(
-            sheet,
-            document.documentElement
-        );
+        document.insertBefore(sheet, document.documentElement);
     }
 
     connectedCallback() {
@@ -166,8 +190,15 @@ class DeveloperDebugPanel extends MozHTMLElement {
 
         this.appendChild(this.elements.app_info);
         this.appendChild(this.elements.proc_info);
-        this.appendChild(this.elements.active_theme);
         this.appendChild(this.elements.user_agent);
+        this.appendChild(
+            html(
+                "div",
+                { class: "dev-active-theme-container" },
+                html("label", {}, "Active Theme:"),
+                this.elements.active_theme
+            )
+        );
 
         this.appendChild(this.elements.graph);
 
@@ -178,18 +209,17 @@ class DeveloperDebugPanel extends MozHTMLElement {
                 window.document.documentElement.style.setProperty(
                     "--height",
                     this.getBoundingClientRect().height + "px"
-                )
+                );
             }).observe(this);
 
-            const devtoolsButton = html("button");
-            devtoolsButton.textContent = "Open Browser Toolbox";
+            const devtoolsButton = html("button", {}, "Open Browser Toolbox");
             devtoolsButton.addEventListener("click", () => {
                 var { BrowserToolboxLauncher } = ChromeUtils.importESModule(
                     "resource://devtools/client/framework/browser-toolbox/Launcher.sys.mjs"
                 );
 
                 BrowserToolboxLauncher.init();
-            })
+            });
 
             this.appendChild(devtoolsButton);
         }
