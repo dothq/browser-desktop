ab71d5fa2e757245d7982d44f8b1629246f93016
EnderDev <kieran@dothq.org>
2023-11-14 21:17:29 +0000

    🐛 Add scope to history-navigation-button to prevent leakage

--- components/navigation/content/browser-history-navigation-button.js
+++ components/navigation/content/browser-history-navigation-button.js
@@ -2,59 +2,62 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const createButton = (direction) =>
-	class extends BrowserToolbarButton {
-		constructor() {
-			super();
+// Scoped to prevent leakage
+{
+	const createButton = (direction) =>
+		class extends BrowserToolbarButton {
+			constructor() {
+				super();
 
-			this.routineId =
-				direction === "back" ? "navigate-back" : "navigate-forward";
-		}
-
-		connectedCallback() {
-			super.connectedCallback();
-
-			window.addEventListener("BrowserTabs::TabSelect", this);
-			window.addEventListener("BrowserTabs::LocationChange", this);
-			window.addEventListener("load", this);
-
-			this.disabled = true;
-		}
-
-		/**
-		 * Handles incoming events
-		 * @param {Event & CustomEvent} event
-		 * @returns
-		 */
-		handleEvent(event) {
-			switch (event.type) {
-				case "load":
-				case "BrowserTabs::LocationChange":
-				case "BrowserTabs::TabSelect":
-					if (!this.context.browser) return;
-
-					if (direction === "back") {
-						this.disabled = !this.context.browser.canGoBack;
-					} else if (direction === "forward") {
-						this.disabled = !this.context.browser.canGoForward;
-					}
-
-					break;
+				this.routineId =
+					direction === "back" ? "navigate-back" : "navigate-forward";
 			}
-		}
 
-		disconnectedCallback() {
-			super.disconnectedCallback();
+			connectedCallback() {
+				super.connectedCallback();
 
-			window.removeEventListener("BrowserTabs::TabSelect", this);
-			window.removeEventListener("BrowserTabs::LocationChange", this);
-			window.removeEventListener("load", this);
-		}
-	};
+				window.addEventListener("BrowserTabs::TabSelect", this);
+				window.addEventListener("BrowserTabs::LocationChange", this);
+				window.addEventListener("load", this);
 
-customElements.define("back-button", createButton("back"), {
-	extends: "button"
-});
-customElements.define("forward-button", createButton("forward"), {
-	extends: "button"
-});
+				this.disabled = true;
+			}
+
+			/**
+			 * Handles incoming events
+			 * @param {Event & CustomEvent} event
+			 * @returns
+			 */
+			handleEvent(event) {
+				switch (event.type) {
+					case "load":
+					case "BrowserTabs::LocationChange":
+					case "BrowserTabs::TabSelect":
+						if (!this.context.browser) return;
+
+						if (direction === "back") {
+							this.disabled = !this.context.browser.canGoBack;
+						} else if (direction === "forward") {
+							this.disabled = !this.context.browser.canGoForward;
+						}
+
+						break;
+				}
+			}
+
+			disconnectedCallback() {
+				super.disconnectedCallback();
+
+				window.removeEventListener("BrowserTabs::TabSelect", this);
+				window.removeEventListener("BrowserTabs::LocationChange", this);
+				window.removeEventListener("load", this);
+			}
+		};
+
+	customElements.define("back-button", createButton("back"), {
+		extends: "button"
+	});
+	customElements.define("forward-button", createButton("forward"), {
+		extends: "button"
+	});
+}
