9945caffeb1d363a8e8cc4de121ac5843c8b126c
EnderDev <kieran@dothq.org>
2023-12-14 20:42:59 +0000

    🎭 Add ability to open context menu inside browser frame

---
+++ actors/DotContextMenuChild.sys.mjs
@@ -0,0 +1,54 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const { ContentDOMReference } = ChromeUtils.importESModule(
+	"resource://gre/modules/ContentDOMReference.sys.mjs"
+);
+
+export class DotContextMenuChild extends JSWindowActorChild {
+	/**
+	 * @param {import("third_party/dothq/gecko-types/lib").ReceiveMessageArgument} message
+	 */
+	receiveMessage(message) {
+		const { targetIdentifier } = message.data;
+
+		switch (message.name) {
+			case "ContextMenu:ReloadFrame": {
+				const { forceReload } = message.data;
+
+				const target = ContentDOMReference.resolve(targetIdentifier);
+
+				/** @type {any} */ (target.ownerDocument.location).reload(
+					forceReload
+				);
+				break;
+			}
+		}
+	}
+
+	/**
+	 * Creates a new context menu context object from an event
+	 * @param {MouseEvent} event
+	 */
+	_createContext(event) {
+		return {};
+	}
+
+	/**
+	 * Receives incoming contextmenu events
+	 * @param {MouseEvent} event
+	 */
+	async handleEvent(event) {
+		console.log("handleEvent", event);
+
+		const context = this._createContext(event);
+
+		this.sendAsyncMessage("contextmenu", {
+			x: event.screenX,
+			y: event.screenY,
+
+			context
+		});
+	}
+}
--- actors/DotContextMenuParent.sys.mjs
+++ actors/DotContextMenuParent.sys.mjs
@@ -2,14 +2,26 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, you can obtain one at http://mozilla.org/MPL/2.0/. */
 
-export class ContextMenuParent extends JSWindowActorParent {
+export class DotContextMenuParent extends JSWindowActorParent {
+	/** @param {import("third_party/dothq/gecko-types/lib").ReceiveMessageArgument<>} message  */
 	receiveMessage(message) {
 		if (message.name !== "contextmenu") return;
 
 		const browser = this.manager.rootFrameLoader.ownerElement;
 		const win = browser.ownerGlobal.top;
 
-		console.log(message, browser, win);
+		const { x, y, context } = message.data;
+
+		const panel = win.gDot.panels.getPanelById("browser-context-menu");
+
+		panel.openPanel({
+			coordMode: "screen",
+
+			x: x,
+			y: y,
+
+			args: context
+		});
 	}
 
 	hiding() {
