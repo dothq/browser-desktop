b8423df17c9a7a33d7192e353403b1faa83f3e63
EnderDev <kieran@dothq.org>
2023-12-15 21:49:11 +0000

    💄 Add dynamic label and icon for add tab command based on supplied URL

--- components/commands/content/add-tab.js
+++ components/commands/content/add-tab.js
@@ -14,21 +14,82 @@ export class AddTabCommand extends Command {
 	constructor(subscription, subscriber, area) {
 		super(subscription, subscriber, area);
 
-		this.label = "New Tab";
-		this.labelAuxiliary = "Open a new tab";
-		this.icon = "add";
+		this._update();
+	}
+
+	/**
+	 * Updates the look of the add tab command
+	 */
+	_update() {
+		const { commandArgs: args } = this.subscriber;
+
+		let label = "New Tab";
+		let labelAuxiliary = "Open a new tab";
+		let icon = "add";
+
+		if (args && args.url) {
+			label = Array.isArray(args.url)
+				? `${args.url.length} tabs`
+				: args.url;
+
+			labelAuxiliary = Array.isArray(args.url)
+				? `Open ${args.url.length} tabs`
+				: `Open ${args.url} in a new tab`;
+
+			if (Array.isArray(args.url)) {
+				label = `${args.url.length} tabs`;
+				labelAuxiliary = `Open ${args.url.length} tabs`;
+				icon = "globe";
+			} else {
+				label = args.url;
+				labelAuxiliary = `Open ${args.url} in a new tab`;
+				icon = `page-icon:${args.url}`;
+			}
+		}
+
+		this.label = label;
+		this.labelAuxiliary = labelAuxiliary;
+		this.icon = icon;
+	}
+
+	/**
+	 * Fired when an attribute on the subscriber is updated
+	 * @param {CustomEvent} event
+	 */
+	on_attributeupdate(event) {
+		switch (event.detail.name) {
+			case "commandargs":
+				this._update();
+				break;
+		}
 	}
 
 	/**
 	 * Fired when the command is performed
 	 *
-	 * @param {import("../Command.sys.mjs").CommandEvent<{}>} event
+	 * @param {import("../Command.sys.mjs").CommandEvent<{ where?: string; url?: string | string[] }>} event
 	 */
 	on_command(event) {
-		const urls = StartPage.getHomePage();
+		const { commandArgs: args } = event.target;
+
+		let urls = [];
+		let where = "tab";
+
+		if (args.url) {
+			if (Array.isArray(args.url)) {
+				urls = args.url;
+				where = "tab";
+			} else {
+				urls = [args.url];
+				where = args.where || "tab";
+			}
+		} else {
+			urls = StartPage.getHomePage();
+			where = "tab";
+		}
 
 		for (const url of urls) {
-			this.actions.run("browser.tabs.open", { where: "tab", url });
+			this.actions.run("browser.tabs.open", { where, url });
 		}
 	}
 }
