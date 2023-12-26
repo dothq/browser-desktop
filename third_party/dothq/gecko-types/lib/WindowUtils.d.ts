115f62284270a031c2efd86eda696e18b0830f10
EnderDev <kieran@dothq.org>
2023-12-26 21:39:32 +0000

    🏷️ Add types for getBoundsWithoutFlushing on WindowUtils

--- third_party/dothq/gecko-types/lib/WindowUtils.d.ts
+++ third_party/dothq/gecko-types/lib/WindowUtils.d.ts
@@ -3,22 +3,37 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 export interface WindowUtils {
-    /**
-     * Returns the number of times this document for this window has
-     * been painted to the screen.
-     */
-    readonly paintCount: number;
+	/**
+	 * Returns the number of times this document for this window has
+	 * been painted to the screen.
+	 */
+	readonly paintCount: number;
 
-    /**
-     * Transform a rectangle given in coordinates relative to this document
-     * to the screen.
-     */
-    toScreenRect(x: number, y: number, width: number, height: number): DOMRect;
+	/**
+	 * Transform a rectangle given in coordinates relative to this document
+	 * to the screen.
+	 */
+	toScreenRect(
+		x: number,
+		y: number,
+		width: number,
+		height: number
+	): DOMRect;
 
-    /**
-     * Transform a rectangle given in coordinates relative to the top level
-     * parent process widget to the local widget. This window should be in a
-     * child process.
-     */
-    convertFromParentProcessWidgetToLocal(x: number, y: number, width: number, height: number): DOMRect;
+	/**
+	 * Transform a rectangle given in coordinates relative to the top level
+	 * parent process widget to the local widget. This window should be in a
+	 * child process.
+	 */
+	convertFromParentProcessWidgetToLocal(
+		x: number,
+		y: number,
+		width: number,
+		height: number
+	): DOMRect;
+
+	/**
+	 * Returns the given element's bounds without flushing pending layout changes.
+	 */
+	getBoundsWithoutFlushing(element: Element): DOMRect;
 }
